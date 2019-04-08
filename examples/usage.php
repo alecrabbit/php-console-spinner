f465822f3ff935e09f8684d9c6ad7c260c752042
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-08 21:30:50 +0300

    wp 15

--- examples/usage.php
+++ examples/usage.php
@@ -4,7 +4,6 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use AlecRabbit\ConsoleColour\Themes;
 use AlecRabbit\Control\Cursor;
-use AlecRabbit\Spinner\ClockSpinner;
 use AlecRabbit\Spinner\Core\AbstractSpinner;
 use AlecRabbit\Spinner\SnakeSpinner;
 
@@ -12,7 +11,7 @@ use AlecRabbit\Spinner\SnakeSpinner;
  * It's a very basic example just to show the concept
  */
 
-const ITERATIONS = 100;
+const ITERATIONS = 300; // Play with this value 100..500
 const MILLISECONDS = 80;
 const MICROSECONDS = MILLISECONDS * 1000;
 const SIMULATED_MESSAGES = [
@@ -31,9 +30,7 @@ const SIMULATED_MESSAGES = [
     95 => 'Be patient',
 ];
 
-//$microseconds = MILLISECONDS * 1000;
-
-// Try different  spinners:
+//// Try different  spinners:
 // CircleSpinner::class
 // ClockSpinner::class
 // MoonSpinner::class
@@ -42,58 +39,61 @@ const SIMULATED_MESSAGES = [
 // DON'T FORGET TO IMPORT!
 $spinnerClass = SnakeSpinner::class;
 
-$simulatedMessages = [
-    0 => 'Initializing',
-    3 => 'Starting',
-    10 => 'Begin processing',
-    12 => 'Gathering data',
-    15 => 'Processing',
-    30 => 'Processing',
-    55 => 'Processing',
-    74 => 'Processing',
-    78 => 'Processing',
-    80 => 'Still processing',
-    88 => 'Still processing',
-    90 => 'Almost there',
-    95 => 'Be patient',
-];
 $theme = new Themes(); // for colored output if supported
 
 echo $theme->comment('Long running task example...') . PHP_EOL;
+echo $theme->dark('Using spinner: ') . $spinnerClass . PHP_EOL;
 
 echo Cursor::hide();
-display(new $spinnerClass(), $theme, true);
-echo PHP_EOL;
-display(new $spinnerClass('processing'), $theme, false);
+
+display(
+    new $spinnerClass(),
+    $theme,
+    true,
+    'Inline Spinner(No custom message):'
+);
+
+display(
+    new $spinnerClass(),
+    $theme,
+    false,
+    'Spinner on the next line(No custom message):' . PHP_EOL
+);
+
+display(
+    new $spinnerClass('processing'),
+    $theme,
+    false,
+    'Spinner on the next line(With custom message "processing"):' . PHP_EOL
+);
 
 echo Cursor::show();
-echo "\007"; // Bell just for fun
 
+echo "\007Bell!" . PHP_EOL; // just for fun
 
 // ************************ Functions ************************
 /**
  * @param AbstractSpinner $s
  * @param Themes $theme
  * @param bool $inline
+ * @param string $message
  */
-function display(AbstractSpinner $s, Themes $theme, bool $inline): void
+function display(AbstractSpinner $s, Themes $theme, bool $inline, string $message): void
 {
-    echo $theme->lightCyan($inline ? 'Inline Spinner' : 'Spinner on the next line') . ':' . PHP_EOL;
-    if (!$inline) {
-        echo PHP_EOL;
-    }
+    echo $theme->lightCyan('Example: ' . $message) . PHP_EOL;
+    $simulatedMessages = getSimulatedMessages();
 
     $s->inline($inline);
     echo $s->begin(); // Optional, begin() is just an alias for spin()
     for ($i = 0; $i < ITERATIONS; $i++) {
         usleep(MICROSECONDS);
-        if (\array_key_exists($i, SIMULATED_MESSAGES)) {
+        if (\array_key_exists($i, $simulatedMessages)) {
             // It's your job to get and echo erase sequence when needed
             echo $s->erase();
             if ($inline) {
                 echo PHP_EOL; // for inline mode
             }
-            echo $theme->none(SIMULATED_MESSAGES[$i] . '...');
+            echo $theme->none($simulatedMessages[$i] . '...');
             if (!$inline) {
                 echo PHP_EOL;
             }
@@ -103,33 +103,21 @@ function display(AbstractSpinner $s, Themes $theme, bool $inline): void
         echo $s->spin($i / ITERATIONS);  // You can pass percentage to spin() method (float from 0 to 1)
     }
     echo $s->end();
-    if (!$inline) {
+    if ($inline) {
         echo PHP_EOL;
     }
-    echo $theme->none('Done!') . PHP_EOL;
+    echo $theme->none('Done!') . PHP_EOL . PHP_EOL;
 }
 
-
-//echo PHP_EOL;
-//echo $theme->comment('Long running task example... (Spinner on the next line)') . PHP_EOL;
-//$s = new $spinnerClass('processing');
-//
-//$s->inline(false);
-//echo PHP_EOL;
-////echo $s->begin(); // Optional
-//for ($i = 0; $i < ITERATIONS; $i++) {
-//    usleep($microseconds);
-//    if (\array_key_exists($i, $simulatedMessages)) {
-//        // It's your job to echo erase sequence when needed
-//        echo $s->erase();
-//        echo $theme->none($simulatedMessages[$i] . '...');
-//        echo PHP_EOL;
-//    }
-//    // It's your job to echo spin() with approx. equal intervals of 80-100ms
-//    // (for comfortable animation only)
-//    echo $s->spin($i / ITERATIONS);
-//}
-//echo $s->end();
-//echo PHP_EOL;
-//echo $theme->none('Done!') . PHP_EOL;
-
+/**
+ * @return array
+ */
+function getSimulatedMessages(): array
+{
+    $c = ITERATIONS / 100;
+    $simulated = [];
+    foreach (SIMULATED_MESSAGES as $key => $message) {
+        $simulated[(int)$key * $c] = $message;
+    }
+    return $simulated;
+}
