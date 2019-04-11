03947655a6c48e3d4bcae3453ce20497b630754b
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-04-11 12:54:27 +0300

    braille

--- examples/demo.php
+++ examples/demo.php
@@ -5,20 +5,24 @@ use AlecRabbit\Control\Cursor;
 use AlecRabbit\Spinner\CircleSpinner;
 use AlecRabbit\Spinner\ClockSpinner;
 use AlecRabbit\Spinner\Contracts\SpinnerInterface;
+use AlecRabbit\Spinner\Contracts\SpinnerStyles;
+use AlecRabbit\Spinner\Contracts\SpinnerSymbols;
 use AlecRabbit\Spinner\Core\Spinner;
+use AlecRabbit\Spinner\Core\Styling;
 use AlecRabbit\Spinner\MoonSpinner;
 use AlecRabbit\Spinner\SimpleSpinner;
 use AlecRabbit\Spinner\SnakeSpinner;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-const ITER = 10;
+const ITER = 50;
 const MESSAGE = 'processing';
 const MESSAGE2 = 'computing';
 
 $theme = new Themes();
 echo Cursor::hide(); //
 echo PHP_EOL;
+sleep(1);
 $spinners = [
     Spinner::class,
     CircleSpinner::class,
@@ -27,12 +31,27 @@ $spinners = [
     SimpleSpinner::class,
     SnakeSpinner::class,
 ];
+
+showSpinners(new class('Message') extends Spinner{
+    protected const ERASING_SHIFT = 2;
+    protected const INTERVAL = 0.25;
+    protected const SYMBOLS = SpinnerSymbols::ZODIAC;
+    protected const
+        STYLES =
+        [
+            Styling::COLOR256_SPINNER_STYLES => SpinnerStyles::C256_RAINBOW,
+            Styling::COLOR_SPINNER_STYLES => SpinnerStyles::C_LIGHT_CYAN,
+        ];
+
+}, true);
 foreach ($spinners as $spinner) {
     showSpinners(new $spinner(MESSAGE), true);
     showSpinners(new $spinner(), true);
     showSpinners(new $spinner(MESSAGE2));
     showSpinners(new $spinner());
 }
+
+
 
 echo PHP_EOL;
 echo PHP_EOL;
