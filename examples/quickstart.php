4c9f47bfc2f54d0073852ca456edca8bca067605
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-08-02 16:35:48 +0300

    Added: `quickstart.php` example

---
+++ examples/quickstart.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use AlecRabbit\Spinner\SnakeSpinner;
+
+const ITERATIONS = 50;
+
+$spinner = new SnakeSpinner();
+
+$spinner->begin();
+for ($i = 0; $i <= ITERATIONS; $i++) {
+    usleep(80000); // Simulating work
+    $spinner->spin();
+}
+$spinner->end();
