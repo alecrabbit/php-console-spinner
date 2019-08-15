285a033b9918ce1fb87f85681cee5b09870868cd
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-08-15 16:44:05 +0300

    example

--- examples/show_all_spinners.php
+++ examples/show_all_spinners.php
@@ -5,6 +5,7 @@ use AlecRabbit\Cli\Tools\Cursor;
 use AlecRabbit\ConsoleColour\Themes;
 use AlecRabbit\Spinner\ArrowSpinner;
 use AlecRabbit\Spinner\BallSpinner;
+use AlecRabbit\Spinner\BlockSpinner;
 use AlecRabbit\Spinner\CircleSpinner;
 use AlecRabbit\Spinner\ClockSpinner;
 use AlecRabbit\Spinner\Core\Spinner;
@@ -25,58 +26,65 @@ const MESSAGE = 'Processing';
 
 $theme = new Themes();
 echo Cursor::hide();
-echo $theme->comment('Spinners samples(with message"' . MESSAGE . '"):') . PHP_EOL;
 
+echo $theme->comment('TimeSpinner(demo time: 10s):') . PHP_EOL;
 showSpinners(
     [
-//        new ArrowSpinner(MESSAGE),
-//        new BallSpinner(MESSAGE),
-//        new CircleSpinner(MESSAGE),
-//        new ClockSpinner(MESSAGE),
-//        new DiceSpinner(MESSAGE),
-//        new DotSpinner(MESSAGE),
-//        new LineSpinner(MESSAGE),
-//        new MoonSpinner(MESSAGE),
-//        new SectorsSpinner(MESSAGE),
-//        new SimpleSpinner(MESSAGE),
-//        new SnakeSpinner(MESSAGE),
-        (new TimeSpinner(MESSAGE))->setTimeFormat('T Y-m-d H:i:s'),
-    ], $theme
+        (new TimeSpinner())->setTimeFormat('T Y-m-d H:i:s'),
+    ],
+    $theme,
+    10
 );
+$classes = [
+    ArrowSpinner::class,
+    BallSpinner::class,
+    BlockSpinner::class,
+    CircleSpinner::class,
+    ClockSpinner::class,
+    DiceSpinner::class,
+    DotSpinner::class,
+    LineSpinner::class,
+    MoonSpinner::class,
+    SectorsSpinner::class,
+    SimpleSpinner::class,
+    SnakeSpinner::class,
+];
+$spinners = [];
+foreach ($classes as $class) {
+    $spinners[] = new $class(MESSAGE);
+}
+echo $theme->comment('Spinners samples(with message"' . MESSAGE . '"):') . PHP_EOL;
+showSpinners($spinners, $theme);
+$spinners = [];
+foreach ($classes as $class) {
+    $spinners[] = new $class();
+}
 echo $theme->comment('Spinners samples(without message):') . PHP_EOL;
-showSpinners(
-    [
-        new ArrowSpinner(),
-        new ClockSpinner(),
-        new MoonSpinner(),
-        new CircleSpinner(),
-        new DiceSpinner(),
-        new SectorsSpinner(),
-        new SimpleSpinner(),
-        new SnakeSpinner(),
-    ], $theme
-);
+showSpinners($spinners, $theme);
+
 echo Cursor::show();
+
 
 // ************************ Functions ************************
 
 /**
  * @param array $spinners
  * @param Themes $theme
+ * @param int $iter
  */
-function showSpinners(array $spinners, Themes $theme): void
+function showSpinners(array $spinners, Themes $theme, int $iter = ITER): void
 {
     /** @var Spinner $s */
     foreach ($spinners as $s) {
         $microseconds = $s->interval() * 1000000;
         echo
-            $theme->cyan('[' . typeOf($s) . '] ') .
-            $theme->dark('(' . Pretty::microseconds($microseconds) . ')') . // Recommended refresh interval
+            $theme->cyan('[' . typeOf($s) . '] ')  .
+            $theme->dark('(Recommended refresh interval: ' . Pretty::microseconds($microseconds) . ')') . // Recommended refresh interval
             PHP_EOL;
-        echo PHP_EOL;
+        echo PHP_EOL, PHP_EOL;
         echo Cursor::up();
         echo $s->begin(); // Optional
-        for ($i = 1; $i <= ITER; $i++) {
+        for ($i = 1; $i <= $iter; $i++) {
             echo $s->spin();
             usleep($microseconds);
         }
