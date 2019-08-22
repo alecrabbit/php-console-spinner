04f67809ff71d79abd58453b6aa9ad2ae689063b
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-08-22 14:19:35 +0300

    add Style::class test

--- tests/Unit/StyleTest.php
+++ tests/Unit/StyleTest.php
@@ -2,28 +2,41 @@
 
 namespace AlecRabbit\Tests\Tools;
 
-use AlecRabbit\Spinner\TimeSpinner;
-use AlecRabbit\Tests\Spinner\Helper;
+use AlecRabbit\Accessories\Circular;
+use AlecRabbit\Spinner\Core\Style;
 use PHPUnit\Framework\TestCase;
-use const AlecRabbit\COLOR256_TERMINAL;
+use function AlecRabbit\Helpers\getValue;
+use const AlecRabbit\NO_COLOR_TERMINAL;
 
 class StyleTest extends TestCase
 {
     protected const PROCESSING = 'Processing';
+    protected const ONE_ELEMENT = 'oneElement';
+    protected const DATA = 'data';
+    protected const PERCENT_STYLES = 'percentStyles';
+    protected const MESSAGE_STYLES = 'messageStyles';
+    protected const SPINNER_STYLES = 'spinnerStyles';
+    protected const S = '%s';
 
     /** @test */
     public function instance(): void
     {
-//        $spinner = new TimeSpinner(self::PROCESSING, false, COLOR256_TERMINAL);
-//        $timeFormat = 'Y-m-d';
-//        $spinner->setTimeFormat($timeFormat);
-//        $this->assertInstanceOf(TimeSpinner::class, $spinner);
-//        $begin = $spinner->begin(0.0);
-//        $this->assertIsString($begin);
-        $this->assertEquals(1, 1);
-//        $this->assertEquals(
-//            '\033[?25l\033[2m' . date($timeFormat) . '...\033[0m\033[2m\033[0m\033[13D',
-//            Helper::stripEscape($begin)
-//        );
+        $style = new Style([], NO_COLOR_TERMINAL);
+        $this->assertInstanceOf(Style::class, $style);
+        $spinnerStyles = getValue($style, self::SPINNER_STYLES);
+        if ($spinnerStyles instanceof Circular) {
+            $this->assertEquals(self::S, getValue($spinnerStyles, self::DATA));
+            $this->assertTrue(getValue($spinnerStyles, self::ONE_ELEMENT));
+        }
+        $messageStyles = getValue($style, self::MESSAGE_STYLES);
+        if ($messageStyles instanceof Circular) {
+            $this->assertEquals(self::S, getValue($messageStyles, self::DATA));
+            $this->assertTrue(getValue($messageStyles, self::ONE_ELEMENT));
+        }
+        $percentStyles = getValue($style, self::PERCENT_STYLES);
+        if ($percentStyles instanceof Circular) {
+            $this->assertEquals(self::S, getValue($percentStyles, self::DATA));
+            $this->assertTrue(getValue($percentStyles, self::ONE_ELEMENT));
+        }
     }
 }
