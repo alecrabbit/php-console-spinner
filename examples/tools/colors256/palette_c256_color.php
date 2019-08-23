4b15aed14fb47f52a78dc2e60544675199dedfbc
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-08-23 09:24:44 +0300

    color tools

--- examples/tools/colors256/palette_c256_color.php
+++ examples/tools/colors256/palette_c256_color.php
@@ -2,6 +2,8 @@
 
 use AlecRabbit\ConsoleColour\ConsoleColor;
 
+const CELL_WIDTH = 7;
+
 require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/../data2.php';
 
@@ -15,7 +17,7 @@ foreach ($colorArray2 as $num => $row) {
     row($row, $c, true);
 
     row($row, $c);
-    if (0 === ($num+1) % 6) {
+    if (0 === ($num + 1) % 6) {
         echo PHP_EOL;
     }
 }
@@ -28,11 +30,24 @@ foreach ($greyScale2 as $num => $row) {
     row($row, $c, false, true);
 }
 
+echo PHP_EOL;
+
+foreach ($xterm2 as $num => $row) {
+    row($row, $c, false, true);
+
+    row($row, $c, true, true);
+
+    row($row, $c, false, true);
+}
+
 function row($row, ConsoleColor $c, $num = false, $bw = false)
 {
     foreach ($row as $index => $color) {
         $textColor = textColor($bw, $color, $index);
-        $text = $num ? '  ' . $color . '  ' : '       ';
+        $text =
+            $num ?
+                str_pad($color, CELL_WIDTH, ' ', STR_PAD_BOTH) :
+                str_repeat(' ', CELL_WIDTH);
         echo $c->apply([$textColor, 'bg_color_' . $color], $text);
     }
     echo PHP_EOL;
@@ -41,8 +56,11 @@ function row($row, ConsoleColor $c, $num = false, $bw = false)
 function textColor($bw, $color, $index): string
 {
     if ($bw) {
-        if ((int)$color > 243) {
+        $i = (int)$color;
+        if ($i > 243) {
             $textColor = 'color_238';
+        } elseif ($i >= 7 && $i <= 15 && $i !== 8) {
+            $textColor = 'color_234';
         } else {
             $textColor = 'color_252';
         }
