6afe3486586443d5b085be0e23fa28727b87b609
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-08-29 14:31:27 +0300

    Added: `EarthSpinner::class`

---
+++ src/Spinner/EarthSpinner.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace AlecRabbit\Spinner;
+
+use AlecRabbit\Spinner\Core\Contracts\Frames;
+use AlecRabbit\Spinner\Core\Contracts\StylesInterface;
+use AlecRabbit\Spinner\Core\Spinner;
+
+class EarthSpinner extends Spinner
+{
+    protected const INTERVAL = 0.2;
+    protected const FRAMES = Frames::EARTH;
+    protected const
+        STYLES =
+        [
+            StylesInterface::SPINNER_STYLES =>
+                [
+                    StylesInterface::COLOR => StylesInterface::C_LIGHT_CYAN,
+                ],
+        ];
+}
