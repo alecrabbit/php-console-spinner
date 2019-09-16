f363bc073e0afcdd5aaea0fbc4ca27cc8b79a03c
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-16 16:15:40 +0300

    example

--- examples/simple.php
+++ examples/simple.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 //require_once __DIR__ . '/../tests/bootstrap.php';
+require_once __DIR__ . '/__include/__functions.php';       // Functions for this demo
 
 use AlecRabbit\Spinner\SnakeSpinner;
 
@@ -19,10 +20,20 @@ $spinner->begin();
 $interval = (int)($spinner->interval() * 1000000);
 for ($i = 0; $i <= ITERATIONS; $i++) {
     usleep($interval); // Simulating work
-    $spinner
-        ->progress($i/ITERATIONS)
-        ->spin();
+//    $spinner
+//        ->progress($i / ITERATIONS)   // You can set progress value separately
+//                                      // from calling spin() method see below
+//        ->spin();
     // or
-    //    $spinner->spin();
+    $spinner->spin();
+    // simulating separate run path
+    if (rnd(100) > 70) {
+        $spinner
+            ->progress($i / ITERATIONS);
+    }
+    if (rnd(100) > 98) {
+        $spinner
+            ->message(date('H:i:s') . ' Refreshed');
+    }
 }
 $spinner->end();
