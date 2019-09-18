82e47b12999081d8e4b55068ff344cee73e51748
Alec The Rabbit <alec.therabbit.git@gmail.com>
2019-09-18 21:43:59 +0300

    formatting

--- src/Spinner/Core/Contracts/OutputInterface.php
+++ src/Spinner/Core/Contracts/OutputInterface.php
@@ -18,7 +18,8 @@ interface OutputInterface
      * Writes a message to the output and adds a newline at the end.
      *
      * @param string|iterable $messages The message as an iterable of strings or a single string
-     * @param int             $options  A bitmask of options (one of the OUTPUT or VERBOSITY constants), 0 is considered the same as self::OUTPUT_NORMAL | self::VERBOSITY_NORMAL
+     * @param int             $options  A bitmask of options (one of the OUTPUT or VERBOSITY constants),
+     * 0 is considered the same as self::OUTPUT_NORMAL | self::VERBOSITY_NORMAL
      */
     public function writeln($messages, $options = 0): void;
 
--- tests/Feature/SpinnerConsistencyTest.php
+++ tests/Feature/SpinnerConsistencyTest.php
@@ -25,12 +25,27 @@ class SpinnerConsistencyTest extends TestCase
         $this->assertEquals('1 Processing... \033[16D', Helper::replaceEscape($spinner->spin()));
         $spinner->progress(0.1);
         $spinner->message('We have here a really long message');
-        $this->assertEquals('2 We have here a really long message... 10% \033[44D', Helper::replaceEscape($spinner->spin()));
-        $this->assertEquals('3 We have here a really long message... 10% \033[44D', Helper::replaceEscape($spinner->spin()));
-        $this->assertEquals('4 We have here a really long message... 10% \033[44D', Helper::replaceEscape($spinner->spin()));
-        $this->assertEquals('1 We have here a really long message... 10% \033[44D', Helper::replaceEscape($spinner->spin()));
+        $this->assertEquals(
+            '2 We have here a really long message... 10% \033[44D',
+            Helper::replaceEscape($spinner->spin())
+        );
+        $this->assertEquals(
+            '3 We have here a really long message... 10% \033[44D',
+            Helper::replaceEscape($spinner->spin())
+        );
+        $this->assertEquals(
+            '4 We have here a really long message... 10% \033[44D',
+            Helper::replaceEscape($spinner->spin())
+        );
+        $this->assertEquals(
+            '1 We have here a really long message... 10% \033[44D',
+            Helper::replaceEscape($spinner->spin())
+        );
         $spinner->message(null);
-        $this->assertEquals('2 10%                                       \033[44D', Helper::replaceEscape($spinner->spin()));
+        $this->assertEquals(
+            '2 10%                                       \033[44D',
+            Helper::replaceEscape($spinner->spin())
+        );
         $this->assertEquals('3 10% \033[6D', Helper::replaceEscape($spinner->spin()));
         $this->assertEquals('4 10% \033[6D', Helper::replaceEscape($spinner->spin()));
         $spinner->progress(null);
@@ -38,7 +53,9 @@ class SpinnerConsistencyTest extends TestCase
         $this->assertEquals('2 \033[2D', Helper::replaceEscape($spinner->spin()));
         $spinner->message(self::PROCESSING);
         $this->assertEquals('3 Processing... \033[16D', Helper::replaceEscape($spinner->spin()));
-        $this->assertEquals('                \033[16D\033[?25h\033[?0c', Helper::replaceEscape($spinner->end()));
+        $this->assertEquals(
+            '                \033[16D\033[?25h\033[?0c',
+            Helper::replaceEscape($spinner->end())
+        );
     }
-
 }
