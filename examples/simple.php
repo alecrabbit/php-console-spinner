d9cfce972cb9914d56a5e720945fb1a749692e26
Alec Rabbit <alec.therabbit.git@gmail.com>
2021-12-12 09:52:26 +0200

    ~wp

--- examples/simple.php
+++ examples/simple.php
@@ -1,4 +1,5 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 use AlecRabbit\Spinner\Spinner;
 use AlecRabbit\Spinner\SpinnerConfig;
@@ -6,9 +7,24 @@ use AlecRabbit\Spinner\SpinnerConfig;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-dump('New begining :)');
+$server = new React\Http\HttpServer(
+    static function (Psr\Http\Message\ServerRequestInterface $request) {
+        return new React\Http\Message\Response(
+            200,
+            array(
+                'Content-Type' => 'text/plain'
+            ),
+            "Hello World!\n"
+        );
+    }
+);
+
+$socket = new React\Socket\Server('0.0.0.0:8080');
+$server->listen($socket);
+
 
 $config = new SpinnerConfig();
 
 $s = new Spinner($config);
 
+echo "Server running at http://0.0.0.0:8080" . PHP_EOL;
