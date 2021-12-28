9684f7f72bdc7e584484ed2346c713284beaae72
Alec Rabbit <alec.therabbit.git@gmail.com>
2021-12-28 20:22:09 +0200

    ~builder

---
+++ src/Spinner/Builder/ConfigBuilder.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Spinner\Builder;
+
+use AlecRabbit\Spinner\Contract\ISpinnerConfig;
+
+final class ConfigBuilder implements Contract\IConfigBuilder
+{
+
+    public function build(): ISpinnerConfig
+    {
+        // TODO: Implement build() method.
+    }
+}
