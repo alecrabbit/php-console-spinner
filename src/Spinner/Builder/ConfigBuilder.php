17655b52e2c5dca32d2aa67e90b1ea73d612daa9
Alec Rabbit <alec.therabbit.git@gmail.com>
2021-12-28 20:25:05 +0200

    ~config builder

--- src/Spinner/Builder/ConfigBuilder.php
+++ src/Spinner/Builder/ConfigBuilder.php
@@ -4,13 +4,67 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Builder;
 
+use AlecRabbit\Spinner\Contract\ILoop;
+use AlecRabbit\Spinner\Contract\IOutput;
 use AlecRabbit\Spinner\Contract\ISpinnerConfig;
+use AlecRabbit\Spinner\Factory\LoopFactory;
+use AlecRabbit\Spinner\SpinnerConfig;
+use AlecRabbit\Spinner\StdErrOutput;
 
-final class ConfigBuilder implements Contract\IConfigBuilder
+final class ConfigBuilder
 {
+    private const DEFAULT_EXIT_MESSAGE = 'Exiting... (CTRL+C to force)';
+    private const DEFAULT_SHUTDOWN_DELAY = 0.5;
+
+    private IOutput $output;
+    private ILoop $loop;
+    private bool $asyncMode;
+
+    public function __construct()
+    {
+        $this->output = new StdErrOutput();
+        $this->asyncMode = true;
+        $this->loop = self::getLoop();
+    }
+
+    private static function refineLoop(ILoop $loop, bool $asyncMode): ?ILoop
+    {
+        if ($asyncMode) {
+            return $loop;
+        }
+        return null;
+    }
+
+    public function withOutput(IOutput $output): self
+    {
+        $this->output = $output;
+        return $this;
+    }
+
+    public function withLoop(ILoop $loop): self
+    {
+        $this->loop = $loop;
+        return $this;
+    }
+
+    public function inSyncMode(): self
+    {
+        $this->asyncMode = false;
+        return $this;
+    }
 
     public function build(): ISpinnerConfig
     {
-        // TODO: Implement build() method.
+        return new SpinnerConfig(
+            output: $this->output,
+            loop:   self::refineLoop($this->loop, $this->asyncMode),
+            async:  $this->asyncMode,
+        );
     }
+
+    private static function getLoop(): ILoop
+    {
+        return LoopFactory::getLoop();
+    }
+
 }
