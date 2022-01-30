9aed8ea97e896ad4382ce7c6d07fee58d9c6e490
Alec Rabbit <alec.therabbit.git@gmail.com>
2022-01-30 11:01:30 +0200

    ~

--- src/Spinner/Core/Contract/Defaults.php
+++ src/Spinner/Core/Contract/Defaults.php
@@ -6,5 +6,10 @@ namespace AlecRabbit\Spinner\Core\Contract;
 
 final class Defaults
 {
+    public const SHUTDOWN_DELAY = 0.5;
+    public const MESSAGE_ON_EXIT = 'Exiting... (CTRL+C to force)';
 
+    private function __construct()
+    {
+    }
 }
