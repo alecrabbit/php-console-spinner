69d56c64d5870519c92d2a84184e41c0d2b4beb2
Alec Rabbit <alec.therabbit.git@gmail.com>
2022-06-07 14:28:15 +0300

    ~

--- src/Spinner/Core/Sequencer.php
+++ src/Spinner/Core/Sequencer.php
@@ -6,17 +6,16 @@ namespace AlecRabbit\Spinner\Core;
 use AlecRabbit\Spinner\Core\Contract\ISequencer;
 
 use const AlecRabbit\Cli\CSI;
-use const AlecRabbit\Cli\ESC;
+use const AlecRabbit\Cli\RESET;
 
 final class Sequencer implements ISequencer
 {
     private const SEQ_HIDE_CURSOR = '?25l';
     private const SEQ_SHOW_CURSOR = '?25h';
-    private const SEQ_RESET = ESC . '[0m';
 
     public function frameSequence(string $sequence): string
     {
-        return CSI . $sequence . self::SEQ_RESET;
+        return CSI . $sequence . RESET;
     }
 
     public function moveBackSequence(int $i = 1): string
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -7,4 +7,5 @@ namespace AlecRabbit\Cli;
 if (!defined(__NAMESPACE__ . '\\' . 'ESC')) {
     define(__NAMESPACE__ . '\\' . 'ESC', "\033");
     define(__NAMESPACE__ . '\\' . 'CSI', ESC . '[');
+    define(__NAMESPACE__ . '\\' . 'RESET', ESC . '[0m');
 }
