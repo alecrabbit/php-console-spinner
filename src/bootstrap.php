3db84f28551fde54024b4817c4cffc9cd9aca63a
Alec Rabbit <alec.therabbit.git@gmail.com>
2022-06-18 19:13:55 +0300

    ~

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Cli;
 
+// @codeCoverageIgnoreStart
 if (!defined(__NAMESPACE__ . '\\' . 'ESC')) {
     define(__NAMESPACE__ . '\\' . 'ESC', "\033");
     define(__NAMESPACE__ . '\\' . 'CSI', ESC . '[');
