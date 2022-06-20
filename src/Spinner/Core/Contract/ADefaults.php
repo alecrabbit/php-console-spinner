bd81bf13cc864fac53229329198a1c41d9bd3dfb
Alec Rabbit <alec.therabbit.git@gmail.com>
2022-06-20 14:06:23 +0300

    ~

---
+++ src/Spinner/Core/Contract/ADefaults.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+// 20.06.22
+namespace AlecRabbit\Spinner\Core\Contract;
+
+use AlecRabbit\Spinner\Kernel\Contract\Base\FramePattern;
+
+use const AlecRabbit\Cli\ALLOWED_TERM_COLOR;
+
+abstract class ADefaults
+{
+    final public const SHUTDOWN_DELAY = 0.15;
+    final public const MAX_SHUTDOWN_DELAY = 10;
+    final public const MESSAGE_ON_EXIT = 'Exiting... (CTRL+C to force)' . PHP_EOL;
+    final public const MESSAGE_INTERRUPTED = 'Interrupted!' . PHP_EOL;
+    final public const FINAL_MESSAGE = PHP_EOL;
+    final public const SPINNER_FRAME_INTERVAL = 0.1;
+    final public const ERASE_WIDTH = 1;
+    final public const FRAME_SEQUENCE = FramePattern::SNAKE_VARIANT_0;
+    final public const MAX_WIDTH = 100;
+    final public const MILLISECONDS_INTERVAL = 1000;
+    final public const COLOR_SUPPORT_LEVELS = ALLOWED_TERM_COLOR;
+    final public const MILLISECONDS_MIN_INTERVAL = 0;
+    final public const MILLISECONDS_MAX_INTERVAL = 1000000;
+    final public const HIDE_CURSOR = true;
+    final public const SYNCHRONOUS_MODE = false;
+
+    protected function __construct()
+    {
+    }
+
+}
--- src/Spinner/Core/Defaults.php
+++ src/Spinner/Core/Defaults.php
@@ -4,30 +4,26 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core;
 
-use AlecRabbit\Spinner\Kernel\Contract\Base\FramePattern;
+use AlecRabbit\Spinner\Core\Contract\ADefaults;
 
-use const AlecRabbit\Cli\ALLOWED_TERM_COLOR;
-use const PHP_EOL;
-
-final class Defaults
+final class Defaults extends ADefaults
 {
-    public const SHUTDOWN_DELAY = 0.15;
-    public const MAX_SHUTDOWN_DELAY = 10;
-    public const MESSAGE_ON_EXIT = 'Exiting... (CTRL+C to force)' . PHP_EOL;
-    public const MESSAGE_INTERRUPTED = 'Interrupted!' . PHP_EOL;
-    public const FINAL_MESSAGE = PHP_EOL;
-    public const SPINNER_FRAME_INTERVAL = 0.1;
-    public const ERASE_WIDTH = 1;
-    public const FRAME_SEQUENCE = FramePattern::SNAKE_VARIANT_0;
-    public const MAX_WIDTH = 100;
-    public const MILLISECONDS_INTERVAL = 1000;
-    public const COLOR_SUPPORT_LEVELS = ALLOWED_TERM_COLOR;
-    public const MILLISECONDS_MIN_INTERVAL = 0;
-    public const MILLISECONDS_MAX_INTERVAL = 1000000;
-    public const HIDE_CURSOR = true;
-    public const SYNCHRONOUS_MODE = false;
+    private static float|int $shutdownDelay = self::SHUTDOWN_DELAY;
+    private static bool $synchronousMode = self::SYNCHRONOUS_MODE;
+    private static bool $hideCursor = self::HIDE_CURSOR;
 
-    private function __construct()
+    public static function shutdownDelay(): float|int
     {
+        return self::$shutdownDelay;
+    }
+
+    public static function synchronousMode(): bool
+    {
+        return self::$synchronousMode;
+    }
+
+    public static function hideCursor(): bool
+    {
+        return self::$hideCursor;
     }
 }
