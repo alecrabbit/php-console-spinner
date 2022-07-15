9422f0863c0c6df74136f57062f23004dda6fae3
Alec Rabbit <alec.therabbit.git@gmail.com>
2022-07-15 23:26:12 +0300

    ~

--- src/Spinner/Spinner.php
+++ src/Spinner/Spinner.php
@@ -5,18 +5,15 @@ declare(strict_types=1);
 namespace AlecRabbit\Spinner;
 
 use AlecRabbit\Spinner\Core\Config\Contract\IConfig;
-use AlecRabbit\Spinner\Core\Contract\ABaseSpinner;
+use AlecRabbit\Spinner\Core\Contract\AMultiSpinner;
 use AlecRabbit\Spinner\Core\Contract\ISpinner;
 use AlecRabbit\Spinner\Core\Twirler\Contract\ITwirler;
 
-final class Spinner extends ABaseSpinner implements ISpinner
+final class Spinner extends AMultiSpinner implements ISpinner
 {
-    private Core\Twirler\Factory\Contract\ITwirlerFactory $twirlerFactory;
-
     public function __construct(IConfig $config)
     {
         parent::__construct($config);
-        $this->twirlerFactory = $config->getTwirlerFactory();
     }
 
     public function spinner(ITwirler|string|null $twirler): void
