5d1d10278388a94df027c5d991c0d7acc9664a0c
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-03 17:25:44 +0200

    ~

--- .make/.bin/gitattributes.sh
+++ .make/.bin/gitattributes.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-
-# The directory to create the .gitattributes file in
-directory=${1:-$(pwd)}
+DIRECTORY=${1:-$(pwd)}
+KEEP_FILE=".gitattributes.keep"
+# Get the list of files and directories to exclude
+EXCLUDES=$(awk '{print $1}' "$KEEP_FILE" | sed 's|^/||')
+# Get the contents of the directory
+CONTENTS=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -exec basename {} \;)
 
 # Create an empty .gitattributes file
-> "$directory/.gitattributes"
-
-# Collect the contents of the directory and exclude those listed in .gitattributes.keep
-contents=$(find "$directory" -mindepth 1 -maxdepth 1 -not -path '*/.gitattributes.keep' -printf '%P\n')
-
+> "$DIRECTORY/.gitattributes"
 # Loop over each file or directory in the contents
 while read -r item; do
-  # Check if the item is a directory
-  if [[ -d "$item" ]]; then
-    # If the item is a directory, add it to the .gitattributes file with the "export-ignore" attribute
-    echo "$item/ export-ignore" >> "$directory/.gitattributes"
-  else
-    # If the item is a file, add it to the .gitattributes file with the "export-ignore" attribute
-    echo "$item export-ignore" >> "$directory/.gitattributes"
-  fi
-done <<< "$contents"
+    if [[ ! "${EXCLUDES[*]}" =~ "${item#./}" ]]; then
+        # Check if the item is a directory
+        if [[ -d "$item" ]]; then
+          # If the item is a directory, add it to the .gitattributes file with the "export-ignore" attribute
+          echo "$item/ export-ignore" >> "$DIRECTORY/.gitattributes"
+        else
+          # If the item is a file, add it to the .gitattributes file with the "export-ignore" attribute
+          echo "$item export-ignore" >> "$DIRECTORY/.gitattributes"
+        fi
+    fi
+done <<< "$CONTENTS"
