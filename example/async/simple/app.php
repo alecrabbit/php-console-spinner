a724512137ef6a3df8f6625f02ff147d55dcbac5
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-03 19:05:22 +0200

    ~

--- example/async/simple/app.php
+++ example/async/simple/app.php
@@ -3,13 +3,12 @@
 declare(strict_types=1);
 
 use AlecRabbit\Spinner\Factory;
-use AlecRabbit\Spinner\Factory\DefaultsFactory;
 
 require_once __DIR__ . '/../bootstrap.async.php';
 
 //// Tune Defaults
 //$defaults =
-//    DefaultsFactory::create()
+//    \AlecRabbit\Spinner\Factory\DefaultsFactory::create()
 //        ->setAttachSignalHandlers(false); // disable signal handling
 
 $spinner = Factory::createSpinner();
