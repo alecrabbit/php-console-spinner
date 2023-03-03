40e59faf14757a01779c45172cf0693c6233416c
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-03 17:48:18 +0200

    ~

--- .make/.bin/gitattributes.sh
+++ .make/.bin/gitattributes.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
+
 DIRECTORY=${1:-$(pwd)}
 KEEP_FILE=".gitattributes.keep"
+
 # Get the list of files and directories to exclude
 EXCLUDES=$(awk '{print $1}' "$KEEP_FILE" | sed 's|^/||')
-# Get the contents of the directory
-CONTENTS=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -exec basename {} \;)
+
+# Get the contents of the directory and sort them alphabetically
+CONTENTS=$(find "$DIRECTORY" -mindepth 1 -maxdepth 1 -exec basename {} \; | sort)
 
 # Create an empty .gitattributes file
 > "$DIRECTORY/.gitattributes"
+
 # Loop over each file or directory in the contents
 while read -r item; do
     if [[ ! "${EXCLUDES[*]}" =~ "${item#./}" ]]; then
