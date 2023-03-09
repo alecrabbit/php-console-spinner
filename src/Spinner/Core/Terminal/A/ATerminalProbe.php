9529eceed2566e79cc9256168afcbc3c26ea33bb
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-09 15:25:41 +0200

    ~wp

--- src/Spinner/Core/Terminal/A/ATerminalProbe.php
+++ src/Spinner/Core/Terminal/A/ATerminalProbe.php
@@ -5,9 +5,12 @@ namespace AlecRabbit\Spinner\Core\Terminal\A;
 
 use AlecRabbit\Spinner\Core\ColorMode;
 use AlecRabbit\Spinner\Core\Terminal\Contract\ITerminalProbe;
+use AlecRabbit\Spinner\Mixin\NoInstanceTrait;
 
 abstract class ATerminalProbe implements ITerminalProbe
 {
+    use NoInstanceTrait;
+
     abstract public static function isSupported(): bool;
 
     abstract public static function getWidth(): int;
