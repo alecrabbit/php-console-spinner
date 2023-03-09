081c250a36349d598dce27ec7f1b2a04eda51c51
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-09 15:26:48 +0200

    ~wp

--- src/Spinner/Core/Loop/A/ALoopProbe.php
+++ src/Spinner/Core/Loop/A/ALoopProbe.php
@@ -6,9 +6,12 @@ namespace AlecRabbit\Spinner\Core\Loop\A;
 
 use AlecRabbit\Spinner\Core\Loop\Contract\ILoop;
 use AlecRabbit\Spinner\Core\Loop\Contract\ILoopProbe;
+use AlecRabbit\Spinner\Mixin\NoInstanceTrait;
 
 abstract class ALoopProbe implements ILoopProbe
 {
+    use NoInstanceTrait;
+
     abstract public static function isSupported(): bool;
 
     abstract public static function createLoop(): ILoop;
