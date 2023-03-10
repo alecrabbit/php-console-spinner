1a7c7fb27000737860864d50f468ae6cf5da5b8d
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-10 13:14:18 +0200

    ~wp

--- example/async/progress/app.php
+++ example/async/progress/app.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use AlecRabbit\Spinner\Core\Interval;
+use AlecRabbit\Spinner\Core\Widget\Contract\IWidgetComposite;
 use AlecRabbit\Spinner\Extras\FractionValue;
+use AlecRabbit\Spinner\Extras\ProgressWidgetFactory;
 use AlecRabbit\Spinner\Factory;
 use AlecRabbit\Spinner\Factory\DefaultsFactory;
 
@@ -11,7 +13,7 @@ require_once __DIR__ . '/../bootstrap.async.php';
 
 // Settings
 $runTime = 30; // s
-$steps = 50;
+$steps = 30;
 $cycleInterval = 0.05; // s
 $progressRefreshInterval = 800; // ms
 $threshold = 900; // 90% [0..1000]
@@ -23,37 +25,13 @@ $count = 0;
 $spinner = Factory::createSpinner();
 $defaults = DefaultsFactory::create();
 
-$progress =
-    new FractionValue(
-        steps: $steps,
-        autoFinish: true
-    );
+$progress = new FractionValue(steps: $steps, autoFinish: true);
 
-$interval =
-    new Interval(
-        $progressRefreshInterval
-    );
+$interval = new Interval($progressRefreshInterval);
 
-$composite = \AlecRabbit\Spinner\Extras\ProgressWidgetFactory::createSteps(
-    $progress,
-    updateInterval: $interval,
-);
+$progressWidget = createProgressWidget($progress, $interval);
 
-$composite->add(
-    \AlecRabbit\Spinner\Extras\ProgressWidgetFactory::createProgressBar(
-        $progress,
-        updateInterval: $interval
-    )
-);
-
-$composite->add(
-    \AlecRabbit\Spinner\Extras\ProgressWidgetFactory::createProgressValue(
-        $progress,
-        updateInterval: $interval
-    )
-);
-
-$spinner->add($composite);
+$spinner->add($progressWidget);
 
 $loop = Factory::getLoop();
 
@@ -85,8 +63,36 @@ $loop->delay(
     static function () use ($spinner, $defaults, $loop) {
         $spinner->finalize('Finished!' . PHP_EOL);
         // Sets shutdown delay
-        $loop->delay($defaults->getShutdownDelay(), static function () use ($loop) {
-            $loop->stop();
-        });
+        $loop->delay(
+            $defaults->getShutdownDelay(),
+            static function () use ($loop) {
+                $loop->stop();
+            }
+        );
     }
 );
+
+function createProgressWidget(
+    FractionValue $progress,
+    Interval $interval
+): IWidgetComposite {
+    $composite = ProgressWidgetFactory::createSteps(
+        $progress,
+        updateInterval: $interval,
+    );
+
+    $composite->add(
+        ProgressWidgetFactory::createProgressBar(
+            $progress,
+            updateInterval: $interval
+        )
+    );
+
+    $composite->add(
+        ProgressWidgetFactory::createProgressValue(
+            $progress,
+            updateInterval: $interval
+        )
+    );
+    return $composite;
+}
