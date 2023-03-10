e7872403a277778f1507cca0ee5bc492948b8c96
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-10 16:39:22 +0200

    ~

---
+++ src/Spinner/Core/Procedure/A/AFloatValueProcedure.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Core\Procedure\A;
+
+use AlecRabbit\Spinner\Core\Contract\IFloatValue;
+use AlecRabbit\Spinner\Core\Contract\IFrame;
+use AlecRabbit\Spinner\Core\Frame;
+use AlecRabbit\Spinner\Core\Procedure\A\AProcedure;
+use AlecRabbit\Spinner\Core\WidthDeterminer;
+
+abstract class AFloatValueProcedure extends AProcedure
+{
+    protected const FORMAT = "%s";
+    protected string $format;
+
+    public function __construct(
+        protected readonly IFloatValue $floatValue,
+        string $format = null,
+    ) {
+        $this->format = $format ?? static::FORMAT;
+    }
+
+    public function update(float $dt = null): IFrame
+    {
+        $v = sprintf(
+            $this->format,
+            $this->floatValue->getValue()
+        );
+        return
+            new Frame($v, WidthDeterminer::determine($v));
+    }
+}
