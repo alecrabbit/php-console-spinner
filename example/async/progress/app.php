e52dec35c9ef13656b1a48dd8ec2fc7554831e1f
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-12 16:27:14 +0200

    ~wp

--- example/async/progress/app.php
+++ example/async/progress/app.php
@@ -25,7 +25,7 @@ $count = 0;
 $defaults = DefaultsFactory::create();
 $defaults
 //    ->setSpinnerCharPattern(new \AlecRabbit\Spinner\Extras\Procedure\TmpProceduralCharPattern())
-    ->setSpinnerCharPattern(new \AlecRabbit\Spinner\Core\Pattern\Char\FeatheredArrow(reversed: true))
+    ->setSpinnerCharPattern(new \AlecRabbit\Spinner\Core\Pattern\Char\Runner(reversed: false))
 //    ->setSpinnerStylePattern(new \AlecRabbit\Spinner\Extras\Procedure\TmpProceduralStylePattern())
 ;
 
