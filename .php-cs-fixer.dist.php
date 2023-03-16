cc148f1672d28646c5489f3e51de5da33b1f9f75
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-16 12:52:51 +0200

    ~wp

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,16 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->exclude('somedir')
+    ->notPath('src/Symfony/Component/Translation/Tests/fixtures/resources.php')
+    ->in(__DIR__)
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR12' => true,
+    'strict_param' => true,
+    'array_syntax' => ['syntax' => 'short'],
+])
+    ->setFinder($finder)
+    ;
