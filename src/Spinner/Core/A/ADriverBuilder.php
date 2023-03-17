b7851b981dbd1e4516c40b517b66a0d65ba93d0c
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-17 12:07:38 +0200

    ~wp

--- src/Spinner/Core/A/ADriverBuilder.php
+++ src/Spinner/Core/A/ADriverBuilder.php
@@ -5,14 +5,25 @@ declare(strict_types=1);
 namespace AlecRabbit\Spinner\Core\A;
 
 use AlecRabbit\Spinner\Contract\IDriver;
+use AlecRabbit\Spinner\Contract\ITimer;
 use AlecRabbit\Spinner\Core\Contract\IDriverBuilder;
 use AlecRabbit\Spinner\Core\Defaults\Contract\IDefaults;
+use AlecRabbit\Spinner\Core\Defaults\Contract\IDriverSettings;
+use AlecRabbit\Spinner\Core\Defaults\Contract\ITerminalSettings;
+use AlecRabbit\Spinner\Core\Driver;
+use AlecRabbit\Spinner\Core\Output\Contract\IOutput;
+use AlecRabbit\Spinner\Core\Output\StreamOutput;
+use AlecRabbit\Spinner\Core\Timer;
 
 abstract class ADriverBuilder implements IDriverBuilder
 {
+    protected ?IDriverSettings $driverSettings = null;
+    protected ?IOutput $output = null;
+    protected ?ITerminalSettings $terminalSettings = null;
+    protected ?ITimer $timer = null;
+
     public function __construct(
         protected IDefaults $defaults,
-
     ) {
     }
 
@@ -21,6 +32,48 @@ abstract class ADriverBuilder implements IDriverBuilder
      */
     public function build(): IDriver
     {
-        // TODO: Implement build() method.
+        $this->output ??= new StreamOutput($this->defaults->getOutputStream());
+        $this->timer ??= new Timer();
+        $this->terminalSettings ??= $this->defaults->getTerminalSettings();
+        $this->driverSettings ??= $this->defaults->getDriverSettings();
+
+        return
+            new Driver(
+                output: $this->output,
+                timer: $this->timer,
+                hideCursor: $this->terminalSettings->isHideCursor(),
+                interruptMessage: $this->driverSettings->getInterruptMessage(),
+                finalMessage: $this->driverSettings->getFinalMessage(),
+            );
     }
+
+    public function withOutput(IOutput $output): static
+    {
+        $clone = clone $this;
+        $clone->output = $output;
+        return $clone;
+    }
+
+    public function withTimer(ITimer $timer): static
+    {
+        $clone = clone $this;
+        $clone->timer = $timer;
+        return $clone;
+    }
+
+    public function withTerminalSettings(ITerminalSettings $terminalSettings): static
+    {
+        $clone = clone $this;
+        $clone->terminalSettings = $terminalSettings;
+        return $clone;
+    }
+
+    public function withDriverSettings(IDriverSettings $driverSettings): static
+    {
+        $clone = clone $this;
+        $clone->driverSettings = $driverSettings;
+        return $clone;
+    }
+
+
 }
