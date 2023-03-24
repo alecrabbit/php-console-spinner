59555787ea66426788cc320a78083a3072d1829a
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-24 12:27:29 +0200

    ~wp

--- src/Spinner/Core/Pattern/Style/WhiteYellow.php
+++ src/Spinner/Core/Pattern/Style/WhiteYellow.php
@@ -11,18 +11,5 @@ final class WhiteYellow extends AStylePattern
 {
     protected const UPDATE_INTERVAL = 800;
 
-    protected const PATTERN = [
-        226,
-        227,
-        228,
-        229,
-        230,
-        231,
-        231,
-        230,
-        229,
-        228,
-        227,
-        226,
-    ];
+    protected const PATTERN = [226, 227, 228, 229, 230, 231, 231, 230, 229, 228, 227, 226,];
 }
