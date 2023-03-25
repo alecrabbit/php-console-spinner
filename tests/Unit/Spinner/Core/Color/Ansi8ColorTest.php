3f0c1155217bb2036f9bb1a592df3c169651e067
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-25 12:38:02 +0200

    ~wp

--- tests/Unit/Spinner/Core/Color/Ansi8ColorTest.php
+++ tests/Unit/Spinner/Core/Color/Ansi8ColorTest.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 // 15.02.23
 namespace AlecRabbit\Tests\Spinner\Unit\Spinner\Core\Color;
 
-use AlecRabbit\Spinner\Contract\IIntNormalizer;
 use AlecRabbit\Spinner\Core\Color\Ansi8Color;
-use AlecRabbit\Spinner\Core\IntNormalizer;
 use AlecRabbit\Spinner\Exception\InvalidArgumentException;
 use AlecRabbit\Tests\Spinner\TestCase\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
@@ -17,6 +15,49 @@ final class Ansi8ColorTest extends TestCase
     public static function DataProvider(): iterable
     {
         // [$expected, $incoming]
+        // #0
+        yield [
+            [
+                self::EXCEPTION => [
+                    self::CLASS_ => InvalidArgumentException::class,
+                    // do not expect specific message
+                ],
+            ],
+            [
+                self::ARGUMENTS => [
+                    self::INDEX => -1,
+                ],
+            ],
+        ];
+        // #1
+        yield [
+            [
+                self::EXCEPTION => [
+                    self::CLASS_ => InvalidArgumentException::class,
+                    // do not expect specific message
+                ],
+            ],
+            [
+                self::ARGUMENTS => [
+                    self::INDEX => 256,
+                ],
+            ],
+        ];
+        // #2
+        yield [
+            [
+                self::EXCEPTION => [
+                    self::CLASS_ => InvalidArgumentException::class,
+                    // do not expect specific message
+                ],
+            ],
+            [
+                self::ARGUMENTS => [
+                    self::HEX => '256',
+                ],
+            ],
+        ];
+        // #3..
         foreach (self::simplifiedDataFeeder() as $item) {
             yield [
                 [
@@ -30,42 +71,25 @@ final class Ansi8ColorTest extends TestCase
                 ],
             ];
         }
-//        yield [
-//            [
-//                self::EXCEPTION => [
-//                    self::CLASS_ => InvalidArgumentException::class,
-//                    self::MESSAGE => 'Divisor should be greater than 0.',
-//                ],
-//            ],
-//            [
-//                self::ARGUMENTS => [
-//                    self::INTERVAL => 100,
-//                    self::DIVISOR => 0,
-//                ],
-//            ],
-//        ];
-//        yield [
-//            [
-//                self::EXCEPTION => [
-//                    self::CLASS_ => InvalidArgumentException::class,
-//                    self::MESSAGE => 'Divisor should be less than 1000000.',
-//                ],
-//            ],
-//            [
-//                self::ARGUMENTS => [
-//                    self::INTERVAL => 100,
-//                    self::DIVISOR => 1200000,
-//                ],
-//            ],
-//        ];
     }
 
     public static function simplifiedDataFeeder(): iterable
     {
+        // #3..
         yield from [
-            // result, index, hex
-            [196, null, '#ff0000',],
+            // result, index, hex // first element - #3
             ['#ff0000', 196, null,],
+            [196, null, '#ff0000',],
+            ['#ff5f87', 204, null,],
+            [204, null, '#ff5f87',],
+            ['#af87ff', 141, null,],
+            [141, null, '#af87ff',],
+            ['#eeeeee', 255, null,],
+            [255, null, '#eeeeee',],
+            ['#808080', 244, null,],
+            [244, null, '#808080',],
+            ['#000000', 16, null,],
+            [16, null, '#000000',], //"#808080" => 244
         ];
     }
 
@@ -77,8 +101,10 @@ final class Ansi8ColorTest extends TestCase
 
         $args = $incoming[self::ARGUMENTS];
 
-        $index = $args[self::INDEX];
-        $hex = $args[self::HEX];
+        $index = $args[self::INDEX] ?? null;
+        $hex = $args[self::HEX] ?? null;
+
+        $this->assertIncomingArguments($index, $hex);
 
         if (null !== $index && null === $hex) {
             $result = Ansi8Color::getHexColor($index);
@@ -88,13 +114,24 @@ final class Ansi8ColorTest extends TestCase
             $result = Ansi8Color::getIndex($hex);
         }
 
-        dump($result, $index, $hex);
-
         if ($expectedException) {
             self::exceptionNotThrown($expectedException);
         }
 
         self::assertSame($expected[self::RESULT], $result);
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     */
+    private function assertIncomingArguments(mixed $index, mixed $hex): void
+    {
+        if (null === $index && null === $hex) {
+            self::fail('Both index and hex arguments are null.');
+        }
+        if (null !== $index && null !== $hex) {
+            self::fail('Both index and hex arguments are not null.');
+        }
     }
 }
 
