7b0df8cd56f7ea31132677471c603f847a6683f9
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-28 15:56:38 +0300

    ~wp

--- tests/Unit/Spinner/Core/A/ASpinnerTest.php
+++ tests/Unit/Spinner/Core/A/ASpinnerTest.php
@@ -9,7 +9,6 @@ use AlecRabbit\Spinner\Core\A\ASpinner;
 use AlecRabbit\Spinner\Core\Contract\ISpinner;
 use AlecRabbit\Spinner\Core\Factory\FrameFactory;
 use AlecRabbit\Spinner\Core\Widget\Contract\IWidgetComposite;
-use AlecRabbit\Spinner\Core\Widget\Contract\IWidgetContext;
 use AlecRabbit\Tests\Spinner\TestCase\TestCase;
 use AlecRabbit\Tests\Spinner\Unit\Spinner\Core\A\Override\ASpinnerOverride;
 use PHPUnit\Framework\Attributes\Test;
@@ -188,8 +187,7 @@ final class ASpinnerTest extends TestCase
         $spinner = $this->getTesteeInstance(driver: $driver, rootWidget: $rootWidget);
 
         $spinner->initialize();
-        /** @noinspection UnnecessaryAssertionInspection */
-        self::assertInstanceOf(IWidgetContext::class, $spinner->add($this->getWidgetCompositeMock()));
+        $spinner->add($this->getWidgetCompositeMock());
 
         self::assertTrue(self::getValue('active', $spinner));
         self::assertFalse(self::getValue('interrupted', $spinner));
