288a34097dfc38cd02fd3785c664ac933805b5d3
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-03-29 14:29:35 +0300

    ~

--- tests/Unit/Spinner/Contract/OptionStyleModeTest.php
+++ tests/Unit/Spinner/Contract/OptionStyleModeTest.php
@@ -9,7 +9,7 @@ use AlecRabbit\Tests\Spinner\TestCase\TestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 
-final class StyleModeTest extends TestCase
+final class OptionStyleModeTest extends TestCase
 {
     public static function lowestDataProvider(): iterable
     {
@@ -47,6 +47,47 @@ final class StyleModeTest extends TestCase
             [OptionStyleMode::NONE, OptionStyleMode::NONE, OptionStyleMode::ANSI8],
             [OptionStyleMode::NONE, OptionStyleMode::NONE, OptionStyleMode::ANSI4],
             [OptionStyleMode::NONE, OptionStyleMode::NONE, OptionStyleMode::ANSI24],
+            [OptionStyleMode::NONE, OptionStyleMode::NONE, null],
+        ];
+    }
+
+    public static function highestDataProvider(): iterable
+    {
+        // [$expected, $incoming]
+        // #0..
+        foreach (self::simplifiedHighestDataFeeder() as $item) {
+            yield [
+                [
+                    self::RESULT => $item[0], // result
+                ],
+                [
+                    self::ARGUMENTS => [
+                        self::MODE_ONE => $item[1],
+                        self::MODE_TWO => $item[2],
+                    ],
+                ],
+            ];
+        }
+    }
+
+    public static function simplifiedHighestDataFeeder(): iterable
+    {
+        // #0..
+        yield from [
+            // result, modeOne, modeTwo // first element - #0..
+            [OptionStyleMode::ANSI8, OptionStyleMode::ANSI8, OptionStyleMode::ANSI8],
+            [OptionStyleMode::ANSI8, OptionStyleMode::ANSI4, OptionStyleMode::ANSI8],
+            [OptionStyleMode::ANSI24, OptionStyleMode::ANSI8, OptionStyleMode::ANSI24],
+            [OptionStyleMode::ANSI24, OptionStyleMode::ANSI24, OptionStyleMode::ANSI8],
+            [OptionStyleMode::ANSI24, OptionStyleMode::ANSI24, OptionStyleMode::ANSI24],
+            [OptionStyleMode::ANSI24, OptionStyleMode::ANSI24, OptionStyleMode::ANSI4],
+            [OptionStyleMode::ANSI4, OptionStyleMode::ANSI4, OptionStyleMode::NONE],
+            [OptionStyleMode::ANSI8, OptionStyleMode::ANSI8, OptionStyleMode::NONE],
+            [OptionStyleMode::ANSI24, OptionStyleMode::ANSI24, OptionStyleMode::NONE],
+            [OptionStyleMode::ANSI8, OptionStyleMode::NONE, OptionStyleMode::ANSI8],
+            [OptionStyleMode::ANSI4, OptionStyleMode::NONE, OptionStyleMode::ANSI4],
+            [OptionStyleMode::ANSI24, OptionStyleMode::NONE, OptionStyleMode::ANSI24],
+            [OptionStyleMode::NONE, OptionStyleMode::NONE, null],
         ];
     }
 
@@ -97,6 +138,24 @@ final class StyleModeTest extends TestCase
         $args = $incoming[self::ARGUMENTS];
 
         $result = $args[self::MODE_ONE]->lowest($args[self::MODE_TWO]);
+
+        if ($expectedException) {
+            self::exceptionNotThrown($expectedException);
+        }
+
+        self::assertSame($expected[self::RESULT]->value, $result->value);
+        self::assertSame($expected[self::RESULT]->name, $result->name);
+    }
+
+    #[Test]
+    #[DataProvider('highestDataProvider')]
+    public function canDetermineHighest(array $expected, array $incoming): void
+    {
+        $expectedException = $this->expectsException($expected);
+
+        $args = $incoming[self::ARGUMENTS];
+
+        $result = $args[self::MODE_ONE]->highest($args[self::MODE_TWO]);
 
         if ($expectedException) {
             self::exceptionNotThrown($expectedException);
