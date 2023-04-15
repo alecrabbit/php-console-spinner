e3cda2052533ad6cff8cc72f1cc1dc7150468572
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-04-15 14:45:16 +0300

    ~

--- src/Spinner/Extras/Color/AnsiColorParser.php
+++ src/Spinner/Extras/Color/AnsiColorParser.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 // 15.04.23
 namespace AlecRabbit\Spinner\Extras\Color;
@@ -9,6 +10,8 @@ use AlecRabbit\Spinner\Extras\Color\Contract\IAnsiColorParser;
 
 final class AnsiColorParser implements IAnsiColorParser
 {
+    protected const FORMAT = '/^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/';
+
     public function __construct(
         protected IHexColorToAnsiCodeConverter $converter,
     ) {
@@ -20,16 +23,21 @@ final class AnsiColorParser implements IAnsiColorParser
             return '';
         }
 
-        if ($this->correctFormat($color)) {
-            return $this->converter->convert($color);
-        }
+        self::assertValid($color);
+        
+        return $this->converter->convert($color);
+    }
 
+    protected static function assertValid(string $color): void
+    {
+        if (self::correctFormat($color)) {
+            return;
+        }
         throw new InvalidArgumentException('Invalid color format: ' . $color);
     }
 
-    private function correctFormat(string $color): bool
+    protected static function correctFormat(string $color): bool
     {
-        return '#' === $color[0];
-//        return preg_match('/^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/', $color);
+        return (bool)preg_match(self::FORMAT, $color);
     }
 }
