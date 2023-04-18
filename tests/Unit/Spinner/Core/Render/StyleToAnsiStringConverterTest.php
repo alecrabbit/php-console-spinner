1331cfc065a5495afd28f22e95d0a071c5d8d31f
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-04-18 16:34:04 +0300

    ~

--- tests/Unit/Spinner/Core/Render/StyleToAnsiStringConverterTest.php
+++ tests/Unit/Spinner/Core/Render/StyleToAnsiStringConverterTest.php
@@ -101,13 +101,68 @@ final class StyleToAnsiStringConverterTest extends TestCaseWithPrebuiltMocksAndS
         $style
             ->expects(self::once())
             ->method('getOptions')
-            ->willReturn(new StyleOptions(StyleOption::BOLD));
-        ;
+            ->willReturn(new StyleOptions(StyleOption::BOLD))
+        ;;
         $style
             ->expects(self::once())
             ->method('getFormat')
             ->willReturn('%s')
         ;
         self::assertSame("\e[31;41;1m%s\e[39;49;22m", $converter->convert($style));
+    }
+
+    #[Test]
+    public function convertReturnsFormatIfStyleIsNotEmptyButNoOptions(): void
+    {
+        $red = '#ff0000';
+        $green = '#00ff00';
+
+        $colorParser = $this->getAnsiColorParserMock();
+        $colorParser
+            ->method('parseColor')
+            ->willReturn('1')
+        ;
+        $optionsParser = $this->getStyleOptionsParserMock();
+        $optionsParser
+            ->method('parseOptions')
+            ->willReturn([['set' => 1, 'unset' => 22]])
+        ;
+        $converter = $this->getTesteeInstance(
+            colorParser: $colorParser,
+            optionsParser: $optionsParser,
+        );
+
+        self::assertInstanceOf(StyleToAnsiStringConverter::class, $converter);
+        $style = $this->getStyleMock();
+        $style
+            ->expects(self::once())
+            ->method('isEmpty')
+            ->willReturn(false)
+        ;
+        $style
+            ->expects(self::once())
+            ->method('getFgColor')
+            ->willReturn($red)
+        ;
+        $style
+            ->expects(self::once())
+            ->method('getBgColor')
+            ->willReturn($green)
+        ;
+        $style
+            ->expects(self::once())
+            ->method('hasOptions')
+            ->willReturn(false)
+        ;
+        $style
+            ->expects(self::never())
+            ->method('getOptions')
+        ;
+        $style
+            ->expects(self::once())
+            ->method('getFormat')
+            ->willReturn('%s')
+        ;
+        self::assertSame("\e[31;41m%s\e[39;49m", $converter->convert($style));
     }
 }
