c8bf91236529e98de28b260210b547b81a9a9b36
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-04-26 14:16:54 +0300

    ~

---
+++ tests/Unit/Spinner/Core/Revolver/Override/ARevolverOverride.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+// 26.04.23
+namespace AlecRabbit\Tests\Unit\Spinner\Core\Revolver\Override;
+
+use AlecRabbit\Spinner\Contract\IFrame;
+use AlecRabbit\Spinner\Contract\IInterval;
+use AlecRabbit\Spinner\Core\Revolver\A\ARevolver;
+
+final class ARevolverOverride extends ARevolver
+{
+    public function __construct(
+        protected IFrame $frame,
+        IInterval $interval,
+        int $deltaTolerance,
+    ) {
+        parent::__construct($interval, $deltaTolerance);
+    }
+
+    protected function next(?float $dt = null): void
+    {
+        $this->frame->sequence(); // simulating frame update
+        $this->frame->width(); // simulating frame update
+    }
+
+    protected function current(): IFrame
+    {
+        return $this->frame;
+    }
+}
