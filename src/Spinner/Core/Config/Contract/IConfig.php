7dfc3bd2abecaa8ed79c0cfe894607cf8345fc87
Oleksii <oleksii.druzhchenko@gmail.com>
2023-07-24 16:07:01 +0300

    ~

--- src/Spinner/Core/Config/Contract/IConfig.php
+++ src/Spinner/Core/Config/Contract/IConfig.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core\Config\Contract;
 
-use AlecRabbit\Spinner\Core\Config\Config;
-
 interface IConfig
 {
     public function getAuxConfig(): IAuxConfig;
