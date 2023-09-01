df9762525ca785acf64ae71607babeb6b8cec2cc
Oleksii <oleksii.druzhchenko@gmail.com>
2023-09-01 16:45:00 +0300

    ~

--- src/Spinner/Core/Config/Contract/Factory/IRootWidgetConfigFactory.php
+++ src/Spinner/Core/Config/Contract/Factory/IRootWidgetConfigFactory.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace AlecRabbit\Spinner\Core\Config\Contract\Factory;
 
 use AlecRabbit\Spinner\Core\Config\Contract\IWidgetConfig;
+use AlecRabbit\Spinner\Core\Settings\Contract\IWidgetSettings;
 
 interface IRootWidgetConfigFactory
 {
-    public function create(): IWidgetConfig;
+    public function create(?IWidgetSettings $widgetSettings = null): IWidgetConfig;
 }
--- src/Spinner/Core/Config/Factory/RootWidgetConfigFactory.php
+++ src/Spinner/Core/Config/Factory/RootWidgetConfigFactory.php
@@ -7,10 +7,17 @@ namespace AlecRabbit\Spinner\Core\Config\Factory;
 use AlecRabbit\Spinner\Core\Config\Contract\Factory\IRootWidgetConfigFactory;
 use AlecRabbit\Spinner\Core\Config\Contract\IWidgetConfig;
 use AlecRabbit\Spinner\Core\Config\WidgetConfig;
+use AlecRabbit\Spinner\Core\Settings\Contract\IWidgetSettings;
 
 final class RootWidgetConfigFactory implements IRootWidgetConfigFactory
 {
-    public function create(): IWidgetConfig
+    public function __construct(
+        // inject solver for WidgetSettings or for individual settings
+    )
+    {
+    }
+
+    public function create(?IWidgetSettings $widgetSettings = null): IWidgetConfig
     {
         return
             new WidgetConfig(
