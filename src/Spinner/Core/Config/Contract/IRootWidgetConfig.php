338ffd16ab6594ae41aa515eadcaac8edfa1a538
Oleksii <oleksii.druzhchenko@gmail.com>
2023-09-14 16:18:20 +0300

    ~

---
+++ src/Spinner/Core/Config/Contract/IRootWidgetConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Spinner\Core\Config\Contract;
+
+use AlecRabbit\Spinner\Contract\IFrame;
+
+interface IRootWidgetConfig extends IWidgetConfig
+{
+
+}
--- tests/Unit/Spinner/Core/Config/ConfigTest.php
+++ tests/Unit/Spinner/Core/Config/ConfigTest.php
@@ -11,11 +11,13 @@ use AlecRabbit\Spinner\Core\Config\Contract\IConfigElement;
 use AlecRabbit\Spinner\Core\Config\Contract\IDriverConfig;
 use AlecRabbit\Spinner\Core\Config\Contract\ILoopConfig;
 use AlecRabbit\Spinner\Core\Config\Contract\IOutputConfig;
+use AlecRabbit\Spinner\Core\Config\Contract\IRootWidgetConfig;
 use AlecRabbit\Spinner\Core\Config\Contract\IWidgetConfig;
 use AlecRabbit\Spinner\Core\Settings\Contract\ISettings;
 use AlecRabbit\Spinner\Exception\InvalidArgumentException;
 use AlecRabbit\Tests\TestCase\TestCaseWithPrebuiltMocksAndStubs;
 use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
 
 final class ConfigTest extends TestCaseWithPrebuiltMocksAndStubs
 {
@@ -33,104 +35,135 @@ final class ConfigTest extends TestCaseWithPrebuiltMocksAndStubs
             new Config();
     }
 
-//    #[Test]
-//    public function canGetAuxConfig(): void
-//    {
-//        $auxConfig = $this->getAuxConfigMock();
-//
-//        $config = $this->getTesteeInstance(
-//            auxConfig: $auxConfig,
-//        );
-//
-//        self::assertSame($auxConfig, $config->getAuxConfig());
-//    }
+    #[Test]
+    public function canSetAndGetAuxConfig(): void
+    {
+        $config = $this->getTesteeInstance();
 
-    protected function getAuxConfigMock(): IAuxConfig
+        $auxConfig = $this->getAuxConfigMock();
+        $auxConfig
+            ->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn(IAuxConfig::class)
+        ;
+
+        $config->set($auxConfig);
+
+        self::assertSame($auxConfig, $config->get(IAuxConfig::class));
+    }
+
+    protected function getAuxConfigMock(): MockObject&IAuxConfig
     {
         return $this->createMock(IAuxConfig::class);
     }
 
-//    #[Test]
-//    public function canGetLoopConfig(): void
-//    {
-//        $loopConfig = $this->getLoopConfigMock();
-//
-//        $config = $this->getTesteeInstance(
-//            loopConfig: $loopConfig,
-//        );
-//
-//        self::assertSame($loopConfig, $config->getLoopConfig());
-//    }
+    #[Test]
+    public function canSetAndGetDriverConfig(): void
+    {
+        $config = $this->getTesteeInstance();
 
-    protected function getLoopConfigMock(): ILoopConfig
+        $driverConfig = $this->getDriverConfigMock();
+        $driverConfig
+            ->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn(IDriverConfig::class)
+        ;
+
+        $config->set($driverConfig);
+
+        self::assertSame($driverConfig, $config->get(IDriverConfig::class));
+    }
+
+    protected function getDriverConfigMock(): MockObject&IDriverConfig
+    {
+        return $this->createMock(IDriverConfig::class);
+    }
+
+
+    #[Test]
+    public function canSetAndGetLoopConfig(): void
+    {
+        $config = $this->getTesteeInstance();
+
+        $loopConfig = $this->getLoopConfigMock();
+        $loopConfig
+            ->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn(ILoopConfig::class)
+        ;
+
+        $config->set($loopConfig);
+
+        self::assertSame($loopConfig, $config->get(ILoopConfig::class));
+    }
+
+
+    protected function getLoopConfigMock(): MockObject&ILoopConfig
     {
         return $this->createMock(ILoopConfig::class);
     }
 
-//    #[Test]
-//    public function canGetOutputConfig(): void
-//    {
-//        $outputConfig = $this->getOutputConfigMock();
-//
-//        $config = $this->getTesteeInstance(
-//            outputConfig: $outputConfig,
-//        );
-//
-//        self::assertSame($outputConfig, $config->getOutputConfig());
-//    }
+    #[Test]
+    public function canSetAndGetOutputConfig(): void
+    {
+        $config = $this->getTesteeInstance();
 
-    protected function getOutputConfigMock(): IOutputConfig
+        $outputConfig = $this->getOutputConfigMock();
+        $outputConfig
+            ->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn(IOutputConfig::class)
+        ;
+
+        $config->set($outputConfig);
+
+        self::assertSame($outputConfig, $config->get(IOutputConfig::class));
+    }
+
+    protected function getOutputConfigMock(): MockObject&IOutputConfig
     {
         return $this->createMock(IOutputConfig::class);
     }
 
     #[Test]
-//    public function canGetDriverConfig(): void
-//    {
-//        $driverConfig = $this->getDriverConfigMock();
-//
-//        $config = $this->getTesteeInstance(
-//            driverConfig: $driverConfig,
-//        );
-//
-//        self::assertSame($driverConfig, $config->getDriverConfig());
-//    }
-
-    protected function getDriverConfigMock(): IDriverConfig
+    public function canSetAndGetWidgetConfig(): void
     {
-        return $this->createMock(IDriverConfig::class);
+        $config = $this->getTesteeInstance();
+
+        $widgetConfig = $this->getWidgetConfigMock();
+        $widgetConfig
+            ->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn(IWidgetConfig::class)
+        ;
+
+        $config->set($widgetConfig);
+
+        self::assertSame($widgetConfig, $config->get(IWidgetConfig::class));
     }
 
-//    #[Test]
-//    public function canGetWidgetConfig(): void
-//    {
-//        $widgetConfig = $this->getWidgetConfigMock();
-//
-//        $config = $this->getTesteeInstance(
-//            widgetConfig: $widgetConfig,
-//        );
-//
-//        self::assertSame($widgetConfig, $config->getWidgetConfig());
-//        self::assertNotSame($widgetConfig, $config->getRootWidgetConfig());
-//    }
-
-    protected function getWidgetConfigMock(): IWidgetConfig
+    protected function getWidgetConfigMock(): MockObject&IWidgetConfig
     {
         return $this->createMock(IWidgetConfig::class);
     }
 
-//    #[Test]
-//    public function canGetRootWidgetConfig(): void
-//    {
-//        $rootWidgetConfig = $this->getWidgetConfigMock();
-//
-//        $config = $this->getTesteeInstance(
-//            rootWidgetConfig: $rootWidgetConfig,
-//        );
-//
-//        self::assertSame($rootWidgetConfig, $config->getRootWidgetConfig());
-//        self::assertNotSame($rootWidgetConfig, $config->getWidgetConfig());
-//    }
+    #[Test]
+    public function canSetAndGetRootWidgetConfig(): void
+    {
+        $config = $this->getTesteeInstance();
+
+        $rootWidgetConfig = $this->getWidgetConfigMock();
+        $rootWidgetConfig
+            ->expects($this->once())
+            ->method('getIdentifier')
+            ->willReturn(IRootWidgetConfig::class)
+        ;
+
+        $config->set($rootWidgetConfig);
+
+        self::assertSame($rootWidgetConfig, $config->get(IRootWidgetConfig::class));
+    }
+
 
     #[Test]
     public function returnsNullIfIdentifierIsNotSet(): void
