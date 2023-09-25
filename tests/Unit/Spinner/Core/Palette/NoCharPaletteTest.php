6f7ba7f7e0fd8da6f4831090912962107125b4ec
Oleksii <oleksii.druzhchenko@gmail.com>
2023-09-25 16:28:40 +0300

    ~

---
+++ tests/Unit/Spinner/Core/Palette/NoCharPaletteTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Unit\Spinner\Core\Palette;
+
+use AlecRabbit\Spinner\Core\CharFrame;
+use AlecRabbit\Spinner\Core\Palette\Contract\IPalette;
+use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
+use AlecRabbit\Spinner\Core\Palette\NoCharPalette;
+use AlecRabbit\Tests\TestCase\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+
+final class NoCharPaletteTest extends TestCase
+{
+    #[Test]
+    public function canBeInstantiated(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        self::assertInstanceOf(NoCharPalette::class, $palette);
+    }
+
+    private function getTesteeInstance(): IPalette
+    {
+        return
+            new NoCharPalette();
+    }
+
+    #[Test]
+    public function canGetEntriesWithOptions(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        $options = $this->getPaletteOptionsMock();
+
+        $entries = $palette->getEntries($options);
+
+        self::assertInstanceOf(\Generator::class, $entries);
+    }
+
+    #[Test]
+    public function canGetEntriesWithoutOptions(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        $entries = $palette->getEntries();
+
+        self::assertInstanceOf(\Generator::class, $entries);
+    }
+
+    private function getPaletteOptionsMock(): MockObject&IPaletteOptions
+    {
+        return $this->createMock(IPaletteOptions::class);
+    }
+
+    #[Test]
+    public function returnsOneFrameIterator(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        $traversable = $palette->getEntries();
+
+        self::assertInstanceOf(\Generator::class, $traversable);
+
+        $entries = iterator_to_array($traversable); // unwrap generator
+
+        self::assertCount(1, $entries);
+        self::assertEquals(new CharFrame('', 0), $entries[0]);
+    }
+}
---
+++ tests/Unit/Spinner/Core/Palette/NoStylePaletteTest.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Tests\Unit\Spinner\Core\Palette;
+
+use AlecRabbit\Spinner\Core\Palette\Contract\IPalette;
+use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
+use AlecRabbit\Spinner\Core\Palette\NoStylePalette;
+use AlecRabbit\Spinner\Core\StyleFrame;
+use AlecRabbit\Tests\TestCase\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\MockObject\MockObject;
+
+final class NoStylePaletteTest extends TestCase
+{
+    #[Test]
+    public function canBeInstantiated(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        self::assertInstanceOf(NoStylePalette::class, $palette);
+    }
+
+    private function getTesteeInstance(): IPalette
+    {
+        return
+            new NoStylePalette();
+    }
+
+    #[Test]
+    public function canGetEntriesWithOptions(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        $options = $this->getPaletteOptionsMock();
+
+        $entries = $palette->getEntries($options);
+
+        self::assertInstanceOf(\Generator::class, $entries);
+    }
+
+    private function getPaletteOptionsMock(): MockObject&IPaletteOptions
+    {
+        return $this->createMock(IPaletteOptions::class);
+    }
+
+    #[Test]
+    public function canGetEntriesWithoutOptions(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        $entries = $palette->getEntries();
+
+        self::assertInstanceOf(\Generator::class, $entries);
+    }
+
+    #[Test]
+    public function returnsOneFrameIterator(): void
+    {
+        $palette = $this->getTesteeInstance();
+
+        $traversable = $palette->getEntries();
+
+        self::assertInstanceOf(\Generator::class, $traversable);
+
+        $entries = iterator_to_array($traversable); // unwrap generator
+
+        self::assertCount(1, $entries);
+        self::assertEquals(new StyleFrame('%s', 0), $entries[0]);
+    }
+}
