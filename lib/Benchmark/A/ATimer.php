47df7d126f5bc1ce17d63df6747107b629e16fe3
Alec Rabbit <alec.therabbit.git@gmail.com>
2023-10-19 14:10:19 +0300

    ~

--- lib/Benchmark/A/ATimer.php
+++ lib/Benchmark/A/ATimer.php
@@ -6,6 +6,9 @@ namespace AlecRabbit\Benchmark\A;
 
 use AlecRabbit\Benchmark\Contract\ITimer;
 use AlecRabbit\Benchmark\Contract\TimeUnit;
+use AlecRabbit\Spinner\Exception\InvalidArgumentException;
+use ReflectionFunction;
+use Throwable;
 
 abstract class ATimer implements ITimer
 {
@@ -18,7 +21,66 @@ abstract class ATimer implements ITimer
 
     protected static function assertTimeFunction(\Closure $timeFunction): void
     {
-        // TODO (2023-10-19 13:40) [Alec Rabbit]: implement assertion
+        try {
+            $timeFunction();
+        } catch (Throwable $e) {
+            throw new InvalidArgumentException(
+                'Invoke of time function throws: ' . $e->getMessage(),
+                previous: $e,
+            );
+        }
+        $reflection = new ReflectionFunction($timeFunction);
+
+        $returnType = $reflection->getReturnType();
+        if ($returnType === null) {
+            throw new InvalidArgumentException(
+                'Return type of time function is not specified.'
+            );
+        }
+        self::assertReturnType($returnType);
+    }
+
+    protected static function assertReturnType(
+        \ReflectionIntersectionType|\ReflectionNamedType|\ReflectionUnionType $returnType
+    ): void {
+        match (true) {
+            $returnType instanceof \ReflectionIntersectionType => self::assertIntersectionType($returnType),
+            $returnType instanceof \ReflectionUnionType => self::assertUnionType($returnType),
+            $returnType instanceof \ReflectionNamedType => self::assertNamedType($returnType),
+        };
+    }
+
+    private static function assertIntersectionType(\ReflectionIntersectionType $intersectionType)
+    {
+        throw new InvalidArgumentException(
+            'Unexpected intersection type.',
+        );
+    }
+
+    private static function assertUnionType(\ReflectionUnionType $unionType)
+    {
+        foreach ($unionType->getTypes() as $type) {
+            self::assertReturnType($type);
+        }
+    }
+
+    private static function assertNamedType(\ReflectionNamedType $namedType)
+    {
+        $type = $namedType->getName();
+        if ($type === 'null' || $namedType->allowsNull()) {
+            throw new InvalidArgumentException(
+                'Time function return type allows null.',
+            );
+        }
+        if ($type !== 'float' && $type !== 'int') {
+            throw new InvalidArgumentException(
+                sprintf(
+                    'Time function must return "int|float"(e.g. "%s"), instead return type is "%s".',
+                    'fn(): int|float => 0.0',
+                    $type,
+                )
+            );
+        }
     }
 
     public function getUnit(): TimeUnit
--- tests/Unit/Benchmark/MicrosecondTimerTest.php
+++ tests/Unit/Benchmark/MicrosecondTimerTest.php
@@ -7,6 +7,8 @@ namespace AlecRabbit\Tests\Unit\Benchmark;
 use AlecRabbit\Benchmark\Contract\ITimer;
 use AlecRabbit\Benchmark\Contract\TimeUnit;
 use AlecRabbit\Benchmark\MicrosecondTimer;
+use AlecRabbit\Spinner\Contract\IInterval;
+use AlecRabbit\Spinner\Exception\InvalidArgumentException;
 use AlecRabbit\Tests\TestCase\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -23,8 +25,7 @@ final class MicrosecondTimerTest extends TestCase
     private function getTesteeInstance(
         ?TimeUnit $unit = null,
         ?\Closure $timeFunction = null,
-    ): ITimer
-    {
+    ): ITimer {
         return
             new MicrosecondTimer(
                 unit: $unit ?? TimeUnit::MINUTE,
@@ -48,12 +49,183 @@ final class MicrosecondTimerTest extends TestCase
     public function canGetNow(): void
     {
         $value = 1.0;
-        $timeFunction = static fn(): float => $value;
+        $timeFunction = static fn(): int|float => $value;
 
         $timer = $this->getTesteeInstance(
             timeFunction: $timeFunction,
         );
 
         self::assertSame($value, $timer->now());
+    }
+
+    #[Test]
+    public function throwsIfTimeFunctionReturnTypeIsValidButActualReturnIsNot(): void
+    {
+        $exceptionClass = InvalidArgumentException::class;
+        $exceptionMessage = 'Invoke of time function throws:';
+
+        $test = function (): void {
+            $timer = $this->getTesteeInstance(
+                timeFunction: static fn(): float => null,
+            );
+
+            self::assertInstanceOf(MicrosecondTimer::class, $timer);
+        };
+
+        $this->wrapExceptionTest(
+            test: $test,
+            exception: $exceptionClass,
+            message: $exceptionMessage,
+        );
+    }
+
+    #[Test]
+    public function throwsIfTimeFunctionReturnTypeAllowsNull(): void
+    {
+        $exceptionClass = InvalidArgumentException::class;
+        $exceptionMessage = 'Time function return type allows null.';
+
+        $test = function (): void {
+            $timer = $this->getTesteeInstance(
+                timeFunction: static fn(): ?float => null,
+            );
+
+            self::assertInstanceOf(MicrosecondTimer::class, $timer);
+        };
+
+        $this->wrapExceptionTest(
+            test: $test,
+            exception: $exceptionClass,
+            message: $exceptionMessage,
+        );
+    }
+
+    #[Test]
+    public function throwsIfTimeFunctionUnionReturnTypeAllowsNull(): void
+    {
+        $exceptionClass = InvalidArgumentException::class;
+        $exceptionMessage = 'Time function return type allows null.';
+
+        $test = function (): void {
+            $timer = $this->getTesteeInstance(
+                timeFunction: static fn(): null|int|float => null,
+            );
+
+            self::assertInstanceOf(MicrosecondTimer::class, $timer);
+        };
+
+        $this->wrapExceptionTest(
+            test: $test,
+            exception: $exceptionClass,
+            message: $exceptionMessage,
+        );
+    }
+
+    #[Test]
+    public function throwsIfTimeFunctionReturnTypeIsIntersection(): void
+    {
+        $exceptionClass = InvalidArgumentException::class;
+        $exceptionMessage = 'Unexpected intersection type.';
+
+        $test = function (): void {
+
+            $intersectionStub = $this->getIntersectionStub();
+
+            $timer = $this->getTesteeInstance(
+                timeFunction: static fn(): ITimer&IInterval => $intersectionStub,
+            );
+
+            self::assertInstanceOf(MicrosecondTimer::class, $timer);
+        };
+
+        $this->wrapExceptionTest(
+            test: $test,
+            exception: $exceptionClass,
+            message: $exceptionMessage,
+        );
+    }
+
+    private function getIntersectionStub(): ITimer&IInterval
+    {
+        return
+            new class implements ITimer, IInterval {
+                public function getUnit(): TimeUnit
+                {
+                    return TimeUnit::SECOND;
+                }
+
+                public function toMicroseconds(): float
+                {
+                    // TODO: Implement toMicroseconds() method.
+                    throw new \RuntimeException('Not implemented.');
+                }
+
+                public function toMilliseconds(): float
+                {
+                    // TODO: Implement toMilliseconds() method.
+                    throw new \RuntimeException('Not implemented.');
+                }
+
+                public function toSeconds(): float
+                {
+                    // TODO: Implement toSeconds() method.
+                    throw new \RuntimeException('Not implemented.');
+                }
+
+                public function smallest(mixed $other): IInterval
+                {
+                    // TODO: Implement smallest() method.
+                    throw new \RuntimeException('Not implemented.');
+                }
+
+                public function now(): int|float
+                {
+                    // TODO: Implement now() method.
+                    throw new \RuntimeException('Not implemented.');
+                }
+            };
+    }
+
+    #[Test]
+    public function throwsIfTimeFunctionReturnTypeIsNotSpecified(): void
+    {
+        $exceptionClass = InvalidArgumentException::class;
+        $exceptionMessage = 'Return type of time function is not specified.';
+
+        $test = function (): void {
+            $timer = $this->getTesteeInstance(
+                timeFunction: static fn() => null,
+            );
+
+            self::assertInstanceOf(MicrosecondTimer::class, $timer);
+        };
+
+        $this->wrapExceptionTest(
+            test: $test,
+            exception: $exceptionClass,
+            message: $exceptionMessage,
+        );
+    }
+
+    #[Test]
+    public function throwsIfTimeFunctionReturnTypeIsInvalid(): void
+    {
+        $exceptionClass = InvalidArgumentException::class;
+        $exceptionMessage =
+            'Time function must return "int|float"(e.g. "fn(): int|float => 0.0"), instead return type is "string".';
+
+        $test = function (): void {
+            $timer = $this->getTesteeInstance(
+                timeFunction: static fn(): string => '-',
+            );
+
+            self::assertInstanceOf(MicrosecondTimer::class, $timer);
+        };
+
+        $this->wrapExceptionTest(
+            test: $test,
+            exception: $exceptionClass,
+            message: $exceptionMessage,
+        );
     }
 }
