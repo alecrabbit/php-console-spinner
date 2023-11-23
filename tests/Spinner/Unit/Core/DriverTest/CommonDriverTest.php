9456cd8d5fbbe3b7a7c1c7df874c701d130c62f3
Oleksii <oleksii.druzhchenko@gmail.com>
2023-11-23 12:41:59 +0200

    ~

--- tests/Spinner/Unit/Core/DriverTest/CommonDriverTest.php
+++ tests/Spinner/Unit/Core/DriverTest/CommonDriverTest.php
@@ -41,58 +41,7 @@ final class CommonDriverTest extends TestCaseForDriver
         $driver->interrupt($interruptMessage);
     }
 
-    // // FIXME (2023-11-21 17:2) [Alec Rabbit]: move to functional tests
-//    #[Test]
-//    public function canAddAndRemoveSpinner(): void
-//    {
-//        $initialInterval = new Interval(2000);
-//
-//        $intervalComparator = $this->getIntervalComparatorMock();
-//
-//        $driver = $this->getTesteeInstance(
-//            initialInterval: $initialInterval,
-//            intervalComparator: $intervalComparator,
-//        );
-//
-//        $intervalOne = new Interval(1200);
-//        $intervalTwo = new Interval(135);
-//
-//        $spinnerOne = $this->getSpinnerStub();
-//        $spinnerOne
-//            ->method('getInterval')
-//            ->willReturn($intervalOne)
-//        ;
-//
-//        $driver->add($spinnerOne);
-//        self::assertSame($spinnerOne, self::getPropertyValue('spinner', $driver));
-//        self::assertSame($intervalOne, $driver->getInterval());
-//
-//        $spinnerTwo = $this->getSpinnerStub();
-//        $spinnerTwo
-//            ->method('getInterval')
-//            ->willReturn($intervalTwo)
-//        ;
-//
-//        $driver->add($spinnerTwo);
-//
-//        self::assertSame($spinnerTwo, self::getPropertyValue('spinner', $driver));
-//        self::assertSame($intervalTwo, $driver->getInterval());
-//
-//        $driver->remove($spinnerTwo);
-//
-//        self::assertNull(self::getPropertyValue('spinner', $driver));
-//        self::assertSame($initialInterval, $driver->getInterval());
-//
-//        $driver->remove($spinnerTwo);
-//
-//        self::assertNull(self::getPropertyValue('spinner', $driver));
-//        self::assertSame($initialInterval, $driver->getInterval());
-//
-//        $driver->remove($spinnerOne);
-//
-//        self::assertNull(self::getPropertyValue('spinner', $driver));
-//        self::assertSame($initialInterval, $driver->getInterval());
-//    }
+
 
     #[Test]
     public function canAddAndRemoveSpinner(): void
