89ac62f7f63e499c1c6c5417e7f94674e42440c0
Oleksii <oleksii.druzhchenko@gmail.com>
2023-12-05 12:23:48 +0200

    ~method deprecation

--- src/Spinner/Core/Contract/ICharFrame.php
+++ src/Spinner/Core/Contract/ICharFrame.php
@@ -8,7 +8,13 @@ use AlecRabbit\Spinner\Contract\IFrame;
 
 interface ICharFrame extends IFrame
 {
+    /**
+     * @deprecated
+     */
     public static function createEmpty(): ICharFrame;
 
+    /**
+     * @deprecated
+     */
     public static function createSpace(): ICharFrame;
 }
