f8330499d559336d2859d86882f1698cbc675888
Oleksii <oleksii.druzhchenko@gmail.com>
2023-12-12 17:56:49 +0200

    ~

--- example/async/server/app.php
+++ example/async/server/app.php
@@ -26,14 +26,23 @@ HTML;
 
 $http = new React\Http\HttpServer(
     static function (Psr\Http\Message\ServerRequestInterface $request) use ($html): React\Http\Message\Response {
+        $uri = $request->getUri();
+
         $datetime = (new DateTimeImmutable())->format(DATE_RFC3339_EXTENDED);
         $info = sprintf(
             '%s %s',
             $request->getMethod(),
-            $request->getUri()
+            $uri
         );
 
-        echo sprintf('%s Request: %s' . PHP_EOL, $datetime, $info);
+        // for simplicity this output is not "wrapped", see IDriver::wrap()
+        echo sprintf('%s %s' . PHP_EOL, $datetime, $info);
+
+        if (str_ends_with($uri->getPath(), 'favicon.ico')) {
+            return React\Http\Message\Response::plaintext('')
+                ->withStatus(Fig\Http\Message\StatusCodeInterface::STATUS_NOT_FOUND)
+            ;
+        }
 
         return React\Http\Message\Response::html(
             sprintf(
@@ -41,20 +50,22 @@ $http = new React\Http\HttpServer(
                 $datetime,
                 $info
             )
-        );
+        )->withStatus(Fig\Http\Message\StatusCodeInterface::STATUS_NOT_FOUND);
     }
 );
+
+$addr = '0.0.0.0:8080';
+
+$http->listen(new React\Socket\SocketServer($addr));
 
 Facade::getLoop()
     ->delay(
         1,
-        static function () use ($http): void {
-            $addr = '0.0.0.0:8080';
-            $http->listen(new React\Socket\SocketServer($addr));
-
+        static function () use ($addr): void {
+            // this is delayed only to prettify output
             echo sprintf('Listening on http://%s' . PHP_EOL, $addr);
         }
     )
 ;
 
-$spinner = Facade::createSpinner();
+Facade::createSpinner(); // yes, assignment to variable is not required 🙂
