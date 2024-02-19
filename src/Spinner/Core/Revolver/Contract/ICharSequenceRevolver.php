969744f8c7d0815d0dc2a54157b4649e69fb04e2
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-19 15:36:06 +0200

    ~

--- src/Spinner/Core/Revolver/Contract/ICharSequenceRevolver.php
+++ src/Spinner/Core/Revolver/Contract/ICharSequenceRevolver.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core\Revolver\Contract;
 
+use AlecRabbit\Spinner\Contract\IHasCharSequenceFrame;
 use AlecRabbit\Spinner\Core\Contract\ICharSequenceFrame;
 
-interface ICharSequenceRevolver extends IRevolver
+interface ICharSequenceRevolver extends IRevolver, IHasCharSequenceFrame
 {
     public function getFrame(?float $dt = null): ICharSequenceFrame;
 }
--- src/Spinner/Core/Revolver/Contract/IStyleSequenceRevolver.php
+++ src/Spinner/Core/Revolver/Contract/IStyleSequenceRevolver.php
@@ -6,9 +6,10 @@ namespace AlecRabbit\Spinner\Core\Revolver\Contract;
 
 use AlecRabbit\Spinner\Contract\IHasInterval;
 use AlecRabbit\Spinner\Contract\IHasSequenceFrame;
+use AlecRabbit\Spinner\Contract\IHasStyleSequenceFrame;
 use AlecRabbit\Spinner\Core\Contract\IStyleSequenceFrame;
 
-interface IStyleSequenceRevolver extends IRevolver
+interface IStyleSequenceRevolver extends IRevolver, IHasStyleSequenceFrame
 {
     public function getFrame(?float $dt = null): IStyleSequenceFrame;
 }
