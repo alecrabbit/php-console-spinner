77f6bf5a8df3f03c61a6a45a49178b256b9e73f5
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-19 16:57:55 +0200

    ~

---
+++ src/Spinner/Core/Widget/Contract/INeoWidgetRevolverBuilder.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Spinner\Core\Widget\Contract;
+
+use AlecRabbit\Spinner\Contract\IHasCharSequenceFrame;
+use AlecRabbit\Spinner\Contract\IHasStyleSequenceFrame;
+use AlecRabbit\Spinner\Contract\IInterval;
+use AlecRabbit\Spinner\Core\Revolver\Contract\IFrameRevolver;
+use AlecRabbit\Spinner\Core\Revolver\Contract\IRevolverBuilder;
+use AlecRabbit\Spinner\Exception\LogicException;
+
+interface INeoWidgetRevolverBuilder extends IRevolverBuilder
+{
+    /**
+     * @throws LogicException
+     */
+    public function build(): IWidgetRevolver;
+
+    public function withChar(IHasCharSequenceFrame $char): INeoWidgetRevolverBuilder;
+
+    public function withStyle(IHasStyleSequenceFrame $style): INeoWidgetRevolverBuilder;
+
+    public function withInterval(IInterval $interval): INeoWidgetRevolverBuilder;
+}
