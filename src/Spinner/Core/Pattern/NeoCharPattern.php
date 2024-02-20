d4eb549dba6d8c524095360f9cb4cf8dee2e3fdb
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-20 15:19:08 +0200

    ~

---
+++ src/Spinner/Core/Pattern/NeoCharPattern.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Spinner\Core\Pattern;
+
+use AlecRabbit\Spinner\Contract\IHasFrame;
+use AlecRabbit\Spinner\Contract\IInterval;
+use AlecRabbit\Spinner\Contract\ICharFrameTransformer;
+use AlecRabbit\Spinner\Contract\ICharSequenceFrame;
+use AlecRabbit\Spinner\Contract\Pattern\INeoCharPattern;
+
+final readonly class NeoCharPattern implements INeoCharPattern
+{
+    public function __construct(
+        private IHasFrame $frames,
+        private IInterval $interval,
+        private ICharFrameTransformer $transformer,
+    ) {
+    }
+
+    public function getInterval(): IInterval
+    {
+        return $this->interval;
+    }
+
+    public function getFrame(?float $dt = null): ICharSequenceFrame
+    {
+        return $this->transformer->transform(
+            $this->frames->getFrame($dt)
+        );
+    }
+}
