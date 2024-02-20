cc12dd5964af19c6a681d1d0fc8f4d4a390dc788
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-20 11:58:55 +0200

    ~fix interfaces

--- src/Spinner/Contract/Pattern/INeoCharPattern.php
+++ src/Spinner/Contract/Pattern/INeoCharPattern.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Contract\Pattern;
 
+use AlecRabbit\Spinner\Contract\ICharSequenceFrame;
 use AlecRabbit\Spinner\Contract\IHasCharSequenceFrame;
 use AlecRabbit\Spinner\Contract\IHasInterval;
 
 interface INeoCharPattern extends INeoPattern, IHasCharSequenceFrame
 {
-
+    /**
+     * @param float|null $dt delta time(milliseconds), time passed since last update
+     */
+    public function getFrame(?float $dt = null): ICharSequenceFrame;
 }
--- src/Spinner/Contract/Pattern/INeoStylePattern.php
+++ src/Spinner/Contract/Pattern/INeoStylePattern.php
@@ -6,8 +6,12 @@ namespace AlecRabbit\Spinner\Contract\Pattern;
 
 use AlecRabbit\Spinner\Contract\IHasInterval;
 use AlecRabbit\Spinner\Contract\IHasStyleSequenceFrame;
+use AlecRabbit\Spinner\Contract\IStyleSequenceFrame;
 
 interface INeoStylePattern extends INeoPattern, IHasStyleSequenceFrame
 {
-
+    /**
+     * @param float|null $dt delta time(milliseconds), time passed since last update
+     */
+    public function getFrame(?float $dt = null): IStyleSequenceFrame;
 }
