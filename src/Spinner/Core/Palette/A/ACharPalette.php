a75eeadeada628a86c8a26a9655d4bebd66bd83f
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-21 16:29:19 +0200

    ~

---
+++ src/Spinner/Core/Palette/A/ACharPalette.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AlecRabbit\Spinner\Core\Palette\A;
+
+use AlecRabbit\Spinner\Contract\ICharSequenceFrame;
+use AlecRabbit\Spinner\Core\Palette\Contract\ICharPalette;
+use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
+
+abstract class ACharPalette implements ICharPalette
+{
+    private int $count;
+
+    public function __construct(
+        private readonly \ArrayObject $frames,
+        private readonly IPaletteOptions $options,
+        private int $index = 0,
+    ) {
+        $this->count = $this->frames->count();
+    }
+
+    public function getFrame(?float $dt = null): ICharSequenceFrame
+    {
+        if ($this->count === 1 || ++$this->index === $this->count) {
+            $this->index = 0;
+        }
+
+        return $this->frames->offsetGet($this->index);
+    }
+
+    public function getOptions(): IPaletteOptions
+    {
+        return $this->options;
+    }
+}
--- src/Spinner/Core/Palette/ModePaletteRenderer.php
+++ src/Spinner/Core/Palette/ModePaletteRenderer.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core\Palette;
 
-use AlecRabbit\Spinner\Contract\IStyleSequenceFrame;
+use AlecRabbit\Spinner\Core\Palette\A\ACharPalette;
+use AlecRabbit\Spinner\Core\Palette\A\AStylePalette;
 use AlecRabbit\Spinner\Core\Palette\Contract\IModePalette;
 use AlecRabbit\Spinner\Core\Palette\Contract\IModePaletteRenderer;
 use AlecRabbit\Spinner\Core\Palette\Contract\IPalette;
-use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
 use AlecRabbit\Spinner\Core\Palette\Contract\IStylePalette;
 use AlecRabbit\Spinner\Core\Palette\Factory\Contract\IPaletteModeFactory;
 
@@ -31,33 +31,11 @@ final readonly class ModePaletteRenderer implements IModePaletteRenderer
         $options = $palette->getOptions($mode);
 
         if ($palette instanceof IStylePalette) {
-            return new class($frames, $options) implements IStylePalette {
-                private int $count;
-
-                public function __construct(
-                    private readonly \ArrayObject $frames,
-                    private readonly IPaletteOptions $options,
-                    private int $index = 0,
-                ) {
-                    $this->count = $this->frames->count();
-                }
-
-                public function getFrame(?float $dt = null): IStyleSequenceFrame
-                {
-                    if ($this->count === 1 || ++$this->index === $this->count) {
-                        $this->index = 0;
-                    }
-
-                    return $this->frames->offsetGet($this->index);
-                }
-
-                public function getOptions(): IPaletteOptions
-                {
-                    return $this->options;
-                }
+            return new class($frames, $options) extends AStylePalette {
             };
         }
 
-        return new NoCharPalette();
+        return new class($frames, $options) extends ACharPalette {
+        };
     }
 }
