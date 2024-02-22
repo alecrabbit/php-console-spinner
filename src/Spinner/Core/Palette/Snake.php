987efaa006421d4afe05260a33dde83ef0da3f69
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-22 17:32:10 +0200

    ~

--- src/Spinner/Core/Palette/Snake.php
+++ src/Spinner/Core/Palette/Snake.php
@@ -4,48 +4,32 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core\Palette;
 
-use AlecRabbit\Spinner\Contract\ICharSequenceFrame;
 use AlecRabbit\Spinner\Core\CharSequenceFrame;
-use AlecRabbit\Spinner\Core\Palette\Contract\ICharPalette;
+use AlecRabbit\Spinner\Core\Palette\A\ACharPalette;
 use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
 use ArrayObject;
 
-final class Snake implements ICharPalette
+final class Snake extends ACharPalette
 {
-    /** @var ArrayObject<int, ICharSequenceFrame> */
-    private ArrayObject $frames;
-    private int $count;
-
     public function __construct(
-        private readonly IPaletteOptions $options = new PaletteOptions(interval: 80),
-        private int $index = 0,
+        IPaletteOptions $options = new PaletteOptions(interval: 80),
+        int $index = 0,
     ) {
-        $this->frames = new ArrayObject(
-            [
-                new CharSequenceFrame('⠏', 1),
-                new CharSequenceFrame('⠛', 1),
-                new CharSequenceFrame('⠹', 1),
-                new CharSequenceFrame('⢸', 1),
-                new CharSequenceFrame('⣰', 1),
-                new CharSequenceFrame('⣤', 1),
-                new CharSequenceFrame('⣆', 1),
-                new CharSequenceFrame('⡇', 1),
-            ],
+        parent::__construct(
+            new ArrayObject(
+                [
+                    new CharSequenceFrame('⠏', 1),
+                    new CharSequenceFrame('⠛', 1),
+                    new CharSequenceFrame('⠹', 1),
+                    new CharSequenceFrame('⢸', 1),
+                    new CharSequenceFrame('⣰', 1),
+                    new CharSequenceFrame('⣤', 1),
+                    new CharSequenceFrame('⣆', 1),
+                    new CharSequenceFrame('⡇', 1),
+                ],
+            ),
+            $options,
+            $index
         );
-        $this->count = $this->frames->count();
-    }
-
-    public function getFrame(?float $dt = null): ICharSequenceFrame
-    {
-        if ($this->count === 1 || ++$this->index === $this->count) {
-            $this->index = 0;
-        }
-
-        return $this->frames->offsetGet($this->index);
-    }
-
-    public function getOptions(): IPaletteOptions
-    {
-        return $this->options;
     }
 }
