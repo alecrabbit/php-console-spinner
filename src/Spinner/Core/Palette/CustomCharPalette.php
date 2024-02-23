9d128e179c49dffa3932725d4f50a3bbc59fe3c0
Oleksii <11045453+alecrabbit@users.noreply.github.com>
2024-02-23 14:10:28 +0200

    ~

--- src/Spinner/Core/Palette/CustomCharPalette.php
+++ src/Spinner/Core/Palette/CustomCharPalette.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core\Palette;
 
+use AlecRabbit\Spinner\Contract\ICharSequenceFrame;
+use AlecRabbit\Spinner\Contract\ISequenceFrame;
+use AlecRabbit\Spinner\Core\CharSequenceFrame;
 use AlecRabbit\Spinner\Core\Palette\A\ACharPalette;
 use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
-use AlecRabbit\Spinner\Extras\Palette\A\AInfiniteCharPalette;
 use ArrayObject;
 
 final class CustomCharPalette extends ACharPalette
@@ -16,6 +18,45 @@ final class CustomCharPalette extends ACharPalette
         IPaletteOptions $options = new PaletteOptions(),
         int $index = 0,
     ) {
-        parent::__construct($frames, $options, $index);
+        parent::__construct(
+            $this->refine($frames),
+            $options,
+            $index,
+        );
+    }
+
+    private function refine(ArrayObject $frames): ArrayObject
+    {
+        $frames = $this->filter($frames);
+
+        if ($frames->count() === 0) {
+            return new ArrayObject(
+                [
+                    $this->getEmptyFrame()
+                ],
+            );
+        }
+
+        return $frames;
+    }
+
+    private function filter(ArrayObject $frames): ArrayObject
+    {
+        return new ArrayObject(
+            array_filter(
+                $frames->getArrayCopy(),
+                $this->getFilterClosure(),
+            )
+        );
+    }
+
+    private function getFilterClosure(): \Closure
+    {
+        return static fn(mixed $frame) => $frame instanceof ICharSequenceFrame;
+    }
+
+    private function getEmptyFrame(): ISequenceFrame
+    {
+        return new CharSequenceFrame('', 0);
     }
 }
--- src/Spinner/Core/Palette/CustomStylePalette.php
+++ src/Spinner/Core/Palette/CustomStylePalette.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace AlecRabbit\Spinner\Core\Palette;
 
+use AlecRabbit\Spinner\Contract\ISequenceFrame;
+use AlecRabbit\Spinner\Contract\IStyleSequenceFrame;
 use AlecRabbit\Spinner\Core\Palette\A\AStylePalette;
 use AlecRabbit\Spinner\Core\Palette\Contract\IPaletteOptions;
+use AlecRabbit\Spinner\Core\StyleSequenceFrame;
 use ArrayObject;
 
 final class CustomStylePalette extends AStylePalette
@@ -15,6 +18,45 @@ final class CustomStylePalette extends AStylePalette
         IPaletteOptions $options = new PaletteOptions(),
         int $index = 0,
     ) {
-        parent::__construct($frames, $options, $index);
+        parent::__construct(
+            $this->refine($frames),
+            $options,
+            $index,
+        );
+    }
+
+    private function refine(ArrayObject $frames): ArrayObject
+    {
+        $frames = $this->filter($frames);
+
+        if ($frames->count() === 0) {
+            return new ArrayObject(
+                [
+                    $this->getEmptyFrame()
+                ],
+            );
+        }
+
+        return $frames;
+    }
+
+    private function filter(ArrayObject $frames): ArrayObject
+    {
+        return new ArrayObject(
+            array_filter(
+                $frames->getArrayCopy(),
+                $this->getFilterClosure(),
+            )
+        );
+    }
+
+    private function getFilterClosure(): \Closure
+    {
+        return static fn(mixed $frame) => $frame instanceof IStyleSequenceFrame;
+    }
+
+    private function getEmptyFrame(): ISequenceFrame
+    {
+        return new StyleSequenceFrame('%s', 0);
     }
 }
